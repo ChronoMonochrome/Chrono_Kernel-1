7da2fc4bdc5c02f03ee3118683749dff26eec881
Thomas Gleixner <tglx@linutronix.de>
2009-07-26 19:39:56 +0200

    rt: Add the preempt-rt lock replacement APIs
    Map spinlocks, rwlocks, rw_semaphores and semaphores to the rt_mutex
    based locking functions for preempt-rt.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- lib/spinlock_debug.c
+++ lib/spinlock_debug.c
@@ -32,6 +32,7 @@ void __raw_spin_lock_init(raw_spinlock_t *lock, const char *name,
 
 EXPORT_SYMBOL(__raw_spin_lock_init);
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 void __rwlock_init(rwlock_t *lock, const char *name,
 		   struct lock_class_key *key)
 {
@@ -47,8 +48,8 @@ void __rwlock_init(rwlock_t *lock, const char *name,
 	lock->owner = SPINLOCK_OWNER_INIT;
 	lock->owner_cpu = -1;
 }
-
 EXPORT_SYMBOL(__rwlock_init);
+#endif
 
 static void spin_bug(raw_spinlock_t *lock, const char *msg)
 {
@@ -156,6 +157,7 @@ void do_raw_spin_unlock(raw_spinlock_t *lock)
 	arch_spin_unlock(&lock->raw_lock);
 }
 
+#ifndef CONFIG_PREEMPT_RT_FULL
 static void rwlock_bug(rwlock_t *lock, const char *msg)
 {
 	if (!debug_locks_off())
@@ -297,3 +299,5 @@ void do_raw_write_unlock(rwlock_t *lock)
 	debug_write_unlock(lock);
 	arch_write_unlock(&lock->raw_lock);
 }
+
+#endif
