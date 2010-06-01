77904d54f03df949b8f319fae0b8ac9e3517c1e8
Linus Walleij <linus.walleij@stericsson.com>
2010-06-01 08:26:49 +0100

    ARM: 6153/1: nomadik MTU to use dynamic shift and mult assignment
    This removes the hard-coded shift values for the MTU timer, since
    the different platforms using this has very different running
    frequencies doing this dynamically is a better idea. Also take
    this opportunity to make a more through shutdown of the MTU
    clockevent when requested.
    
    Acked-by: Alessandro Rubini <rubini@unipv.it>
    Signed-off-by: Linus Walleij <linus.walleij@stericsson.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/plat-nomadik/include/plat/mtu.h
+++ arch/arm/plat-nomadik/include/plat/mtu.h
@@ -5,6 +5,15 @@ void nmdk_timer_init(void __iomem *base);
 void nmdk_clkevt_reset(void);
 void nmdk_clksrc_reset(void);
 
+/*
+ * Guaranteed runtime conversion range in seconds for
+ * the clocksource and clockevent.
+ */
+#define MTU_MIN_RANGE 4
+
+/* should be set by the platform code */
+extern void __iomem *mtu_base;
+
 struct clock_event_device *nmdk_clkevt_get(void);
 
 #endif /* __PLAT_MTU_H */
