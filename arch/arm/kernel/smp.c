f056e01dcf46e1eaa261d60a604b35c6f023b6ff
Mian Yousaf Kaukab <mian.yousaf.kaukab@stericsson.com>
2010-06-22 11:37:20 +0200

    fix build errors with CPU_IDLE enabled
    Current PM for Montblanc enables both LOCAL_TIMERS and
    GENERIC_CLOCKEVENTS_BROADCAST. This causes build error with
    v2.6.34. This patch fixes this error. The PM scheme should be reviewed
    for v2.6.34.
    
    Signed-off-by: Mian Yousaf Kaukab <mian.yousaf.kaukab@stericsson.com>
    Change-Id: Icdafd6f60cec858fc1311614be2e053bba168ef3
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/2257
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -423,7 +423,7 @@ static void ipi_timer(void)
 }
 
 #ifdef CONFIG_GENERIC_CLOCKEVENTS_BROADCAST
-static void smp_timer_broadcast(const struct cpumask *mask)
+void smp_timer_broadcast(const struct cpumask *mask)
 {
 	smp_cross_call(mask, IPI_TIMER);
 }
