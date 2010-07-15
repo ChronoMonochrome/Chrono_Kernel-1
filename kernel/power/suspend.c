e179aa4375d8d40a4acb2a6820072fcfbc466e3e
Thomas Gleixner <tglx@linutronix.de>
2010-07-15 10:29:00 +0200

    suspend: Prevent might sleep splats
    timekeeping suspend/resume calls read_persistant_clock() which takes
    rtc_lock. That results in might sleep warnings because at that point
    we run with interrupts disabled.
    
    We cannot convert rtc_lock to a raw spinlock as that would trigger
    other might sleep warnings.
    
    As a temporary workaround we disable the might sleep warnings by
    setting system_state to SYSTEM_SUSPEND before calling sysdev_suspend()
    and restoring it to SYSTEM_RUNNING afer sysdev_resume().
    
    Needs to be revisited.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -176,6 +176,8 @@ static int suspend_enter(suspend_state_t state)
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
 
+	system_state = SYSTEM_SUSPEND;
+
 	error = syscore_suspend();
 	if (!error) {
 		if (!(suspend_test(TEST_CORE) || pm_wakeup_pending())) {
@@ -184,6 +186,8 @@ static int suspend_enter(suspend_state_t state)
 		}
 		syscore_resume();
 	}
+
+	system_state = SYSTEM_RUNNING;
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
