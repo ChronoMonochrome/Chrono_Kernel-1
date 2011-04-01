d97a724aa74880d781d43edf6a6f0b1f10824ed5
Len Brown <len.brown@intel.com>
2011-04-01 19:34:59 -0400

    cpuidle: stop depending on pm_idle
    cpuidle users should call cpuidle_call_idle() directly
    rather than via (pm_idle)() function pointer.
    
    Architecture may choose to continue using (pm_idle)(),
    but cpuidle need not depend on it:
    
      my_arch_cpu_idle()
    	...
    	if(cpuidle_call_idle())
    		pm_idle();
    
    cc: Kevin Hilman <khilman@deeprootsystems.com>
    cc: Paul Mundt <lethal@linux-sh.org>
    cc: x86@kernel.org
    Acked-by: H. Peter Anvin <hpa@linux.intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>
    
    Conflicts:
    	arch/arm/kernel/process.c
    	arch/sh/kernel/idle.c
    	arch/x86/kernel/process_32.c
    	arch/x86/kernel/process_64.c
    
    Conflicts:
    	drivers/cpuidle/cpuidle.c

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -32,6 +32,7 @@
 #include <linux/hw_breakpoint.h>
 #include <linux/console.h>
 #include <linux/leds.h>
+#include <linux/cpuidle.h>
 
 #include <asm/cacheflush.h>
 #include <asm/idmap.h>
@@ -264,8 +265,8 @@ void cpu_idle(void)
 				cpu_relax();
 			} else if (!need_resched()) {
 				stop_critical_timings();
-
-				pm_idle();
+				if (cpuidle_call_idle())
+					pm_idle();
 				start_critical_timings();
 				/*
 				 * pm_idle functions must always
