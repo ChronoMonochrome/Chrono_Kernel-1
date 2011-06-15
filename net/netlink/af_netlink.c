85dde6794b03a0a6e0df9a10a6ea5ea9c313a441
Dan Carpenter <error27@gmail.com>
2011-06-15 03:11:42 +0000

    rtnetlink: unlock on error path in netlink_dump()
    In c7ac8679bec939 "rtnetlink: Compute and store minimum ifinfo dump
    size", we moved the allocation under the lock so we need to unlock
    on error path.
    
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Signed-off-by: David S. Miller <davem@conan.davemloft.net>

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1712,7 +1712,7 @@ static int netlink_dump(struct sock *sk)
 
 	skb = sock_rmalloc(sk, alloc_size, 0, GFP_KERNEL);
 	if (!skb)
-		goto errout;
+		goto errout_skb;
 
 	len = cb->dump(skb, cb);
 
@@ -1750,7 +1750,6 @@ static int netlink_dump(struct sock *sk)
 errout_skb:
 	mutex_unlock(nlk->cb_mutex);
 	kfree_skb(skb);
-errout:
 	return err;
 }
 
