45b14878748dda3f47ebd758a60f95ac426d15ad
Paul Bolle <pebolle@tiscali.nl>
2011-07-24 17:58:46 +0200

    eventpoll: fix comment typo 'evenpoll'
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Conflicts:
    	fs/eventpoll.c

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -296,6 +296,7 @@ ctl_table epoll_table[] = {
 
 static const struct file_operations eventpoll_fops;
 
+/* Fast test to see if the file is an eventpoll file */
 static inline int is_file_epoll(struct file *f)
 {
 	return f->f_op == &eventpoll_fops;
