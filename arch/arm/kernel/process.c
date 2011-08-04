3fdf9e03ed47e2e2b24dd76e8378d1ab8aef1439
David Brown <davidb@codeaurora.org>
2011-08-04 09:24:31 -0700

    cpuidle: Consistent spelling of cpuidle_idle_call()
    Commit a0bfa1373859e9d11dc92561a8667588803e42d8 mispells
    cpuidle_idle_call() on ARM and SH code.  Fix this to be consistent.
    
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Paul Mundt <lethal@linux-sh.org>
    Cc: x86@kernel.org
    Cc: Len Brown <len.brown@intel.com>
    Signed-off-by: David Brown <davidb@codeaurora.org>
    [ Also done by Mark Brown - th ebug has been around forever, and was
      noticed in -next, but the idle tree never picked it up. Bad bad bad ]
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	arch/sh/kernel/idle.c

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -277,7 +277,7 @@ void cpu_idle(void)
 				cpu_relax();
 			} else if (!need_resched()) {
 				stop_critical_timings();
-				if (cpuidle_call_idle())
+				if (cpuidle_idle_call())
 					pm_idle();
 				start_critical_timings();
 				/*
