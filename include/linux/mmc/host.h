510712d086c2ffe38af3745ea924317b5d7acd68
Per Forlin <per.forlin@linaro.org>
2011-08-19 14:52:37 +0200

    mmc: core: add random fault injection
    This adds support to inject data errors after a completed host transfer.
    The mmc core will return error even though the host transfer is successful.
    This simple fault injection proved to be very useful to test the
    non-blocking error handling in the mmc_blk_issue_rw_rq().
    Random faults can also test how the host driver handles pre_req()
    and post_req() in case of errors.
    
    Signed-off-by: Per Forlin <per.forlin@linaro.org>
    Acked-by: Akinobu Mita <akinobu.mita@gmail.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -13,6 +13,7 @@
 #include <linux/leds.h>
 #include <linux/sched.h>
 #include <linux/wakelock.h>
+#include <linux/fault-inject.h>
 
 #include <linux/mmc/core.h>
 #include <linux/mmc/pm.h>
@@ -316,6 +317,10 @@ struct mmc_host {
 	} embedded_sdio_data;
 #endif
 	struct mmc_async_req	*areq;		/* active async req */
+
+#ifdef CONFIG_FAIL_MMC_REQUEST
+	struct fault_attr	fail_mmc_request;
+#endif
 
 	unsigned long		private[0] ____cacheline_aligned;
 };
