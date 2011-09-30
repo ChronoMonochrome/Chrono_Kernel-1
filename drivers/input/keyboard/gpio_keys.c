f6649f844157e19c7045c1c437de88c5fbb2dc6a
Jonas Aaberg <jonas.aberg@stericsson.com>
2011-09-30 13:10:26 +0200

    input: gpio-keys: Disable hw on suspend
    Disable hw if active when suspending if the hw can't wake
    the system from suspend.
    
    ST-Ericsson Linux next: -
    ST-Ericsson ID: -
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: Ieaf2a2022b23782bc19afa152dc5e3fa5a0024ad
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32721
    Reviewed-by: Bengt JONSSON <bengt.g.jonsson@stericsson.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -39,6 +39,8 @@ struct gpio_keys_drvdata {
 	struct input_dev *input;
 	struct mutex disable_lock;
 	unsigned int n_buttons;
+	bool enabled;
+	bool enable_after_suspend;
 	int (*enable)(struct device *dev);
 	void (*disable)(struct device *dev);
 	struct gpio_button_data data[0];
@@ -434,6 +436,7 @@ static int gpio_keys_open(struct input_dev *input)
 {
 	struct gpio_keys_drvdata *ddata = input_get_drvdata(input);
 
+	ddata->enabled = true;
 	return ddata->enable ? ddata->enable(input->dev.parent) : 0;
 }
 
@@ -443,6 +446,7 @@ static void gpio_keys_close(struct input_dev *input)
 
 	if (ddata->disable)
 		ddata->disable(input->dev.parent);
+	ddata->enabled = false;
 }
 
 static int __devinit gpio_keys_probe(struct platform_device *pdev)
@@ -468,6 +472,7 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 	ddata->n_buttons = pdata->nbuttons;
 	ddata->enable = pdata->enable;
 	ddata->disable = pdata->disable;
+	ddata->enabled = false;
 	mutex_init(&ddata->disable_lock);
 
 	platform_set_drvdata(pdev, ddata);
@@ -589,6 +594,10 @@ static int gpio_keys_suspend(struct device *dev)
 				enable_irq_wake(irq);
 			}
 		}
+	} else {
+		ddata->enable_after_suspend = ddata->enabled;
+		if (ddata->enabled)
+			gpio_keys_close(ddata->input);
 	}
 
 	return 0;
@@ -611,6 +620,10 @@ static int gpio_keys_resume(struct device *dev)
 
 		gpio_keys_report_event(&ddata->data[i]);
 	}
+
+	if (!device_may_wakeup(dev) && ddata->enable_after_suspend)
+		gpio_keys_open(ddata->input);
+
 	input_sync(ddata->input);
 
 	return 0;
