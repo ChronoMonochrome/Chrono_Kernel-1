8fcf37473122cf9ff36b101d8a662d3aefbf6887
Eliad Peller <eliad@wizery.com>
2011-10-02 10:15:52 +0200

    mac80211: pass vif param to conf_tx() callback
    tx params should be configured per interface.
    add ieee80211_vif param to the conf_tx callback,
    and change all the drivers that use this callback.
    
    The following spatch was used:
    @rule1@
    struct ieee80211_ops ops;
    identifier conf_tx_op;
    @@
    	ops.conf_tx = conf_tx_op;
    
    @rule2@
    identifier rule1.conf_tx_op;
    identifier hw, queue, params;
    @@
    	conf_tx_op (
    -		struct ieee80211_hw *hw,
    +		struct ieee80211_hw *hw, struct ieee80211_vif *vif,
    		u16 queue,
    		const struct ieee80211_tx_queue_params *params) {...}
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    
    Conflicts:
    	drivers/net/wireless/iwlwifi/iwl-core.c
    	drivers/staging/brcm80211/brcmsmac/mac80211_if.c

--- drivers/net/wireless/rt2x00/rt2400pci.c
+++ drivers/net/wireless/rt2x00/rt2400pci.c
@@ -1664,7 +1664,8 @@ static int rt2400pci_probe_hw(struct rt2x00_dev *rt2x00dev)
 /*
  * IEEE80211 stack callback functions.
  */
-static int rt2400pci_conf_tx(struct ieee80211_hw *hw, u16 queue,
+static int rt2400pci_conf_tx(struct ieee80211_hw *hw,
+			     struct ieee80211_vif *vif, u16 queue,
 			     const struct ieee80211_tx_queue_params *params)
 {
 	struct rt2x00_dev *rt2x00dev = hw->priv;
@@ -1677,7 +1678,7 @@ static int rt2400pci_conf_tx(struct ieee80211_hw *hw, u16 queue,
 	if (queue != 0)
 		return -EINVAL;
 
-	if (rt2x00mac_conf_tx(hw, queue, params))
+	if (rt2x00mac_conf_tx(hw, vif, queue, params))
 		return -EINVAL;
 
 	/*
