8698630af02c58d99cf407cd87d7f30e8895bed7
Avinash A <Avinash.a@stericsson.com>
2011-10-03 19:51:13 +0530

    mach-ux500: keypad support for sony display
    This add the platform related changes to
    support the keypad for sony displays
    
    ST-Ericsson ID: 362724
    ST-Ericsson Linux next: NA
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: Ibb0ead5a76dff423e00b1c3d169a9d20d140d1c3
    Signed-off-by: Avinash A <Avinash.a@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/32506
    Reviewed-by: Avinash A <avinash.a@stericsson.com>
    Tested-by: Avinash A <avinash.a@stericsson.com>
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>

--- arch/arm/mach-ux500/board-mop500-cyttsp.c
+++ arch/arm/mach-ux500/board-mop500-cyttsp.c
@@ -9,12 +9,16 @@
 #include <linux/gpio.h>
 #include <linux/cyttsp.h>
 #include <linux/delay.h>
+#include <linux/gpio/nomadik.h>
+#include <linux/i2c.h>
+#include <linux/input/matrix_keypad.h>
+#include <linux/mfd/tc3589x.h>
+#include <linux/mfd/dbx500-prcmu.h>
 #include <linux/amba/pl022.h>
 #include <plat/pincfg.h>
 #include <mach/hardware.h>
 #include <mach/irqs.h>
 #include <mach/irqs-db8500.h>
-#include <linux/mfd/dbx500-prcmu.h>
 #include "pins-db8500.h"
 #include "board-mop500.h"
 #include "devices-db8500.h"
@@ -137,6 +141,63 @@ static struct spi_board_info cypress_spi_devices[] = {
 	}
 };
 
+/*
+ * TC35893
+ */
+static const unsigned int sony_keymap[] = {
+	KEY(3, 1, KEY_END),
+	KEY(4, 1, KEY_HOME),
+	KEY(6, 4, KEY_VOLUMEDOWN),
+	KEY(4, 2, KEY_EMAIL),
+	KEY(3, 3, KEY_RIGHT),
+	KEY(2, 5, KEY_BACKSPACE),
+
+	KEY(6, 7, KEY_MENU),
+	KEY(5, 0, KEY_ENTER),
+	KEY(4, 3, KEY_0),
+	KEY(3, 4, KEY_DOT),
+	KEY(5, 2, KEY_UP),
+	KEY(3, 5, KEY_DOWN),
+
+	KEY(4, 5, KEY_SEND),
+	KEY(0, 5, KEY_BACK),
+	KEY(6, 2, KEY_VOLUMEUP),
+	KEY(1, 3, KEY_SPACE),
+	KEY(7, 6, KEY_LEFT),
+	KEY(5, 5, KEY_SEARCH),
+};
+
+static struct matrix_keymap_data sony_keymap_data = {
+	.keymap		= sony_keymap,
+	.keymap_size    = ARRAY_SIZE(sony_keymap),
+};
+
+static struct tc3589x_keypad_platform_data tc35893_data = {
+	.krow = TC_KPD_ROWS,
+	.kcol = TC_KPD_COLUMNS,
+	.debounce_period = TC_KPD_DEBOUNCE_PERIOD,
+	.settle_time = TC_KPD_SETTLE_TIME,
+	.irqtype = IRQF_TRIGGER_FALLING,
+	.enable_wakeup = true,
+	.keymap_data    = &sony_keymap_data,
+	.no_autorepeat  = true,
+};
+
+static struct tc3589x_platform_data tc3589x_keypad_data = {
+	.block = TC3589x_BLOCK_KEYPAD,
+	.keypad = &tc35893_data,
+	.irq_base = MOP500_EGPIO_IRQ_BASE,
+};
+
+static struct i2c_board_info __initdata mop500_i2c0_devices_u8500[] = {
+	{
+		I2C_BOARD_INFO("tc3589x", 0x44),
+		.platform_data = &tc3589x_keypad_data,
+		.irq = NOMADIK_GPIO_TO_IRQ(64),
+		.flags = I2C_CLIENT_WAKE,
+	},
+};
+
 void mop500_cyttsp_init(void)
 {
 	int ret = 0;
@@ -157,4 +218,9 @@ void __init mop500_u8500uib_r3_init(void)
 {
 	mop500_cyttsp_init();
 	db8500_add_spi2(&mop500_spi2_data);
+	nmk_config_pin((GPIO64_GPIO     | PIN_INPUT_PULLUP), false);
+	mop500_uib_i2c_add(0, mop500_i2c0_devices_u8500,
+			ARRAY_SIZE(mop500_i2c0_devices_u8500));
+	mop500_uib_i2c_add(0, mop500_i2c0_devices_u8500,
+			ARRAY_SIZE(mop500_i2c0_devices_u8500));
 }
