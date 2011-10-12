a965b58036cb7433fff1ac19f7f6a4ed55df01b6
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-12 14:32:10 +0200

    ske keypad: Proper regulator and clock framework support
    SKE keypad doesn't block the APSleep during CPU Idle
    by proper implementation of regulator and clock support for
    SKE.
    
    ST-Ericsson Id: ER 323894
    
    Change-Id: I242f9618439e3004b5458981235b7079a97aa7dd
    Signed-off-by: Naveen Kumar Gaddipati <naveen.gaddipati@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/20127
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>

--- arch/arm/plat-nomadik/include/plat/ske.h
+++ arch/arm/plat-nomadik/include/plat/ske.h
@@ -22,6 +22,9 @@
 #define SKE_MIS		0x18
 #define SKE_ICR		0x1C
 
+#define SKE_KPD_MAX_ROWS        8
+#define SKE_KPD_MAX_COLS        8
+
 /*
  * Keypad module
  */
@@ -30,21 +33,27 @@
  * struct keypad_platform_data - structure for platform specific data
  * @init:	pointer to keypad init function
  * @exit:	pointer to keypad deinitialisation function
+ * @gpio_input_pins:	pointer to gpio input pins
+ * @gpio_output_pins:	pointer to gpio output pins
  * @keymap_data: matrix scan code table for keycodes
  * @krow:	maximum number of rows
  * @kcol:	maximum number of columns
  * @debounce_ms: platform specific debounce time
  * @no_autorepeat: flag for auto repetition
  * @wakeup_enable: allow waking up the system
+ * @switch_delay: gpio switch_delay
  */
 struct ske_keypad_platform_data {
 	int (*init)(void);
 	int (*exit)(void);
+	int *gpio_input_pins;
+	int *gpio_output_pins;
 	const struct matrix_keymap_data *keymap_data;
 	u8 krow;
 	u8 kcol;
 	u8 debounce_ms;
 	bool no_autorepeat;
 	bool wakeup_enable;
+	int switch_delay;
 };
 #endif	/*__SKE_KPD_H*/
