be3df14833b59d1c80553a750d6a824d7aa38564
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-12 11:05:06 +0200

    mach-ux500: Fix for MMC working on u5500 and DMA support.
    Configuring DMA support on u5500 for MMC.
    
    ST Ericsson ID:WP257117
    Change-Id: I00c45e47e77a6a472e8eb8471a64e163036cd8c8
    Signed-off-by: seshagh <seshagiri.holi@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/18722
    Reviewed-by: Preetham-rao K <preetham.rao@stericsson.com>
    Tested-by: Preetham-rao K <preetham.rao@stericsson.com>
    
    Conflicts:
    
    	arch/arm/mach-ux500/Makefile
    	arch/arm/mach-ux500/board-u5500-sdi.c
    	arch/arm/mach-ux500/board-u5500.c

---
+++ arch/arm/mach-ux500/board-u5500.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2010
+ *
+ * License terms: GNU General Public License (GPL) version 2
+ */
+
+#ifndef __BOARD_U5500_H
+#define __BOARD_U5500_H
+
+#define GPIO_SDMMC_CD          180
+#define GPIO_MMC_CARD_CTRL     227
+#define GPIO_MMC_CARD_VSEL     185
+
+#endif
