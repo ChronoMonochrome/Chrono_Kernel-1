fd0ba2d130f49f50be42c1ceb70c49e80daa0eac
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-13 16:24:46 +0200

    mach-ux500: gpio: Moved the NOMADIK_NR_GPIO
    Move the NOMADIK_NR_GPIO to the right
    location.
    
    Fix warnings generated by this change.
    
    ST-Ericsson Linux next: ER 340134
    ST-Ericsson ID: 340139
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Signed-off-by: Robert Marklund <robert.marklund@stericsson.com>
    Change-Id: I5b69d4a4662b707d815a867b2b5be19e4675d010
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/24082
    Reviewed-by: Philippe LANGLAIS <philippe.langlais@stericsson.com>

--- arch/arm/mach-ux500/include/mach/gpio.h
+++ arch/arm/mach-ux500/include/mach/gpio.h
@@ -1,5 +1,17 @@
 #ifndef __ASM_ARCH_GPIO_H
 #define __ASM_ARCH_GPIO_H
 
+/*
+ * 288 (#267 is the highest one actually hooked up) onchip GPIOs, plus enough
+ * room for a couple of GPIO expanders.
+ */
+
+#if CONFIG_ARCH_NR_GPIO > 0
+#define ARCH_NR_GPIOS CONFIG_ARCH_NR_GPIO
+#else
+#define ARCH_NR_GPIOS	355
+#endif
+
+#define NOMADIK_NR_GPIO	288
 
 #endif /* __ASM_ARCH_GPIO_H */
