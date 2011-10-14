471d0b85a246d6d4a55122f527c5359ef9bd7294
Marcus Lorentzon <marcus.xm.lorentzon@stericsson.com>
2011-10-14 16:47:53 +0200

    ux500: mcde: Remove unnecessary MCDE fbdev update
    The first fbdev update in board-xxx-mcde.c is not
    needed. Screen will be updated at first fbdev pan.
    
    ST-Ericsson ID: 365249, 361835
    ST-Ericsson FOSS-OUT ID: Trivial
    ST-Ericsson Linux next: NA
    
    Change-Id: I8055448e9be967530b783734473a32d579ba74fd
    Signed-off-by: Marcus Lorentzon <marcus.xm.lorentzon@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/34120
    Reviewed-by: QATOOLS
    Reviewed-by: Jayarami REDDY <jayarami.reddy@stericsson.com>
    Reviewed-by: Jimmy RUBIN <jimmy.rubin@stericsson.com>

--- arch/arm/mach-ux500/board-mop500-mcde.c
+++ arch/arm/mach-ux500/board-mop500-mcde.c
@@ -548,46 +548,6 @@ static struct notifier_block display_nb = {
 	.notifier_call = display_postregistered_callback,
 };
 
-/*
-* This function is used to refresh the display (lcd, hdmi, tvout) with black
-* when the framebuffer is registered.
-* The main display will not be updated if startup graphics is displayed
-* from u-boot.
-*/
-static int framebuffer_postregistered_callback(struct notifier_block *nb,
-	unsigned long event, void *data)
-{
-	int ret = 0;
-	struct fb_event *event_data = data;
-	struct fb_info *info;
-	struct fb_var_screeninfo var;
-	struct fb_fix_screeninfo fix;
-	struct mcde_fb *mfb;
-
-	if (event != FB_EVENT_FB_REGISTERED)
-		return 0;
-
-	if (!event_data)
-		return 0;
-
-	info = event_data->info;
-	mfb = to_mcde_fb(info);
-	if (mfb->id == 0 && display_initialized_during_boot)
-		goto out;
-
-	var = info->var;
-	fix = info->fix;
-	var.yoffset = var.yoffset ? 0 : var.yres;
-	if (info->fbops->fb_pan_display)
-		ret = info->fbops->fb_pan_display(&var, info);
-out:
-	return ret;
-}
-
-static struct notifier_block framebuffer_nb = {
-	.notifier_call = framebuffer_postregistered_callback,
-};
-
 static void setup_primary_display(void)
 {
 	/* Display reset GPIO is different depending on reference boards */
@@ -609,10 +569,6 @@ int __init init_display_devices(void)
 
 	if (!cpu_is_u8500())
 		return ret;
-
-	ret = fb_register_client(&framebuffer_nb);
-	if (ret)
-		pr_warning("Failed to register framebuffer notifier\n");
 
 	ret = mcde_dss_register_notifier(&display_nb);
 	if (ret)
