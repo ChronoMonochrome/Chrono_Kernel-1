330fe086efcda1d61c2b1afbc32bd7068508d344
Philippe Langlais <philippe.langlais@linaro.org>
2011-10-19 22:00:28 +0200

    mach-ux500: hotplug update for pm
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/mach-ux500/hotplug.c
+++ arch/arm/mach-ux500/hotplug.c
@@ -11,20 +11,31 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/smp.h>
+#include <linux/completion.h>
 
 #include <asm/cacheflush.h>
 #include <asm/smp_plat.h>
 
+#include <mach/context.h>
+
 extern volatile int pen_release;
+
+static DECLARE_COMPLETION(cpu_killed);
 
 static inline void platform_do_lowpower(unsigned int cpu)
 {
 	flush_cache_all();
 
-	/* we put the platform to just WFI */
 	for (;;) {
-		__asm__ __volatile__("dsb\n\t" "wfi\n\t"
-				: : : "memory");
+
+		context_varm_save_core();
+		context_save_cpu_registers();
+
+		context_save_to_sram_and_wfi(false);
+
+		context_restore_cpu_registers();
+		context_varm_restore_core();
+
 		if (pen_release == cpu_logical_map(cpu)) {
 			/*
 			 * OK, proper wakeup, we're done
@@ -36,7 +47,7 @@ static inline void platform_do_lowpower(unsigned int cpu)
 
 int platform_cpu_kill(unsigned int cpu)
 {
-	return 1;
+	return wait_for_completion_timeout(&cpu_killed, 5000);
 }
 
 /*
@@ -46,6 +57,19 @@ int platform_cpu_kill(unsigned int cpu)
  */
 void platform_cpu_die(unsigned int cpu)
 {
+#ifdef DEBUG
+	unsigned int this_cpu = hard_smp_processor_id();
+
+	if (cpu != this_cpu) {
+		printk(KERN_CRIT "Eek! platform_cpu_die running on %u, should be %u\n",
+			   this_cpu, cpu);
+		BUG();
+	}
+#endif
+
+	printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
+	complete(&cpu_killed);
+
 	/* directly enter low power state, skipping secure registers */
 	platform_do_lowpower(cpu);
 }
