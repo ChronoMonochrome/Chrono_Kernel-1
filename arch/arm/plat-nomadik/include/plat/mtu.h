213266e1676e0dafb7fd945d3b7d033c227e88df
Philippe Langlais <philippe.langlais@linaro.org>
2011-10-19 22:07:34 +0200

    mach-ux500: Change for cpuidle support
    Signed-off-by: Philippe Langlais <philippe.langlais@linaro.org>

--- arch/arm/plat-nomadik/include/plat/mtu.h
+++ arch/arm/plat-nomadik/include/plat/mtu.h
@@ -5,5 +5,7 @@ void nmdk_timer_init(void __iomem *base);
 void nmdk_clkevt_reset(void);
 void nmdk_clksrc_reset(void);
 
+struct clock_event_device *nmdk_clkevt_get(void);
+
 #endif /* __PLAT_MTU_H */
 
