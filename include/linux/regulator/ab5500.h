d60cd0c87f8d118d9fbbdb87e218acd731f1c850
Philippe Langlais <philippe.langlais@stericsson.com>
2011-10-19 15:48:25 +0200

    regulator: ab5500: add ab5500 regulator driver
    Signed-off-by: Philippe Langlais <philippe.langlais@stericsson.com>

---
+++ include/linux/regulator/ab5500.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) ST-Ericsson SA 2011
+ *
+ * License Terms: GNU General Public License v2
+ */
+
+#ifndef __LINUX_REGULATOR_AB5500_H
+#define __LINUX_REGULATOR_AB5500_H
+
+enum ab5500_regulator_id {
+	AB5500_LDO_G,
+	AB5500_LDO_H,
+	AB5500_LDO_K,
+	AB5500_LDO_L,
+	AB5500_LDO_VDIGMIC,
+	AB5500_LDO_SIM,
+	AB5500_BIAS2,
+	AB5500_NUM_REGULATORS,
+};
+
+struct regulator_init_data;
+
+struct ab5500_regulator_platform_data {
+	struct regulator_init_data *regulator;
+	int num_regulator;
+};
+
+#endif
