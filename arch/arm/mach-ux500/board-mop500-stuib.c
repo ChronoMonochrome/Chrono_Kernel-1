4648164ba5eb4dc894da6a8ba130cb71c64aedaf
Robert Marklund <robert.marklund@stericsson.com>
2011-10-26 15:17:44 +0200

    ux500: Separate sensors and stuib
    Separate sensors and stuib so we can start one
    without the other. Snowball want sensors but
    dont have any uib.
    
    Change-Id: I96fd1051c0a85c5e8eccd83b08076b7348d4fc17
    Signed-off-by: Robert Marklund <robert.marklund@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/35357
    Reviewed-by: Philippe LANGLAIS <philippe.langlais@stericsson.com>

--- arch/arm/mach-ux500/board-mop500-stuib.c
+++ arch/arm/mach-ux500/board-mop500-stuib.c
@@ -10,66 +10,11 @@
 #include <linux/input/bu21013.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
-#include <linux/lsm303dlh.h>
-#include <linux/l3g4200d.h>
 #include <linux/i2c.h>
 #include <linux/input/matrix_keypad.h>
-#include <linux/input/lps001wp.h>
 #include <asm/mach-types.h>
 
 #include "board-mop500.h"
-
-/*
- * LSM303DLH accelerometer + magnetometer sensors
- */
-static struct lsm303dlh_platform_data __initdata lsm303dlh_pdata= {
-	.name_a = "lsm303dlh.0",
-	.name_m = "lsm303dlh.1",
-	.axis_map_x = 0,
-	.axis_map_y = 1,
-	.axis_map_z = 2,
-	.negative_x = 1,
-	.negative_y = 1,
-	.negative_z = 0,
-};
-
-static struct l3g4200d_gyr_platform_data  __initdata l3g4200d_pdata_u8500 = {
-	.name_gyr = "l3g4200d",
-	.axis_map_x = 1,
-	.axis_map_y = 0,
-	.axis_map_z = 2,
-	.negative_x = 0,
-	.negative_y = 0,
-	.negative_z = 1,
-};
-
-static struct lps001wp_prs_platform_data __initdata lps001wp_pdata = {
-	.poll_interval = 500,
-	.min_interval = 10,
-};
-
-static struct i2c_board_info __initdata mop500_i2c2_devices[] = {
-	{
-		/* LSM303DLH Accelerometer */
-		I2C_BOARD_INFO("lsm303dlh_a", 0x18),
-		.platform_data = &lsm303dlh_pdata,
-	},
-	{
-		/* LSM303DLH Magnetometer */
-		I2C_BOARD_INFO("lsm303dlh_m", 0x1E),
-		.platform_data = &lsm303dlh_pdata,
-	},
-	{
-		/* L3G4200D Gyroscope */
-		I2C_BOARD_INFO("l3g4200d", 0x68),
-		.platform_data = &l3g4200d_pdata_u8500,
-	},
-	{
-		/* LSP001WM Barometer */
-		I2C_BOARD_INFO("lps001wp_prs_sysfs", 0x5C),
-		.platform_data = &lps001wp_pdata,
-	},
-};
 
 /*
  * ux500 keymaps
@@ -304,20 +249,5 @@ void __init mop500_stuib_init(void)
 	mop500_uib_i2c_add(3, u8500_i2c3_devices_stuib,
 			ARRAY_SIZE(u8500_i2c3_devices_stuib));
 
-	if (machine_is_hrefv60()) {
-		lsm303dlh_pdata.irq_a1 = HREFV60_ACCEL_INT1_GPIO;
-		lsm303dlh_pdata.irq_a2 = HREFV60_ACCEL_INT2_GPIO;
-		lsm303dlh_pdata.irq_m = HREFV60_MAGNET_DRDY_GPIO;
-	}
-	else if (machine_is_snowball()) {
-		lsm303dlh_pdata.irq_a1 = SNOWBALL_ACCEL_INT1_GPIO;
-		lsm303dlh_pdata.irq_a2 = SNOWBALL_ACCEL_INT2_GPIO;
-		lsm303dlh_pdata.irq_m = SNOWBALL_MAGNET_DRDY_GPIO;
-	} else {
-		lsm303dlh_pdata.irq_a1 = GPIO_ACCEL_INT1;
-		lsm303dlh_pdata.irq_a2 = GPIO_ACCEL_INT2;
-		lsm303dlh_pdata.irq_m = GPIO_MAGNET_DRDY;
-	}
-	mop500_uib_i2c_add(2, mop500_i2c2_devices,
-			ARRAY_SIZE(mop500_i2c2_devices));
+	mop500_sensors_init();
 }
