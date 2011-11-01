70155ad0794c4d0395f501cc16491039913b0d75
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-01 10:15:27 +0000

    ARM: restart: remove argument to setup_mm_for_reboot()
    setup_mm_for_reboot() doesn't make use of its argument, so remove it.
    
    Acked-by: Nicolas Pitre <nico@linaro.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Acked-by: H Hartley Sweeten <hsweeten@visionengravers.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/idmap.c
+++ arch/arm/mm/idmap.c
@@ -104,7 +104,7 @@ arch_initcall(init_static_idmap);
  * the user-mode pages.  This will then ensure that we have predictable
  * results when turning the mmu off
  */
-void setup_mm_for_reboot(char mode)
+void setup_mm_for_reboot(void)
 {
 	/*
 	 * We need to access to user-mode page tables here. For kernel threads
