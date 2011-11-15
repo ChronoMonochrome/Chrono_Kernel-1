76f377bd4a4189929b0322e018c21073c364fea2
Pawel Szyszuk <pawel.szyszuk@stericsson.com>
2011-11-15 13:45:08 +0000

    U9500: Add function cpu_is_u9500()
    ST-Ericsson Linux next: NA
    ST-Ericsson ID: 365660
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I5bb0cc9f4721a9b3b46334913609d5a301cbb9e0
    Signed-off-by: Pawel Szyszuk <pawel.szyszuk@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/37834
    Reviewed-by: Derek MORTON <derek.morton@stericsson.com>
    Reviewed-by: QATOOLS
    Reviewed-by: QABUILD
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Reviewed-by: Srinidhi KASAGAR <srinidhi.kasagar@stericsson.com>

--- arch/arm/mach-ux500/include/mach/id.h
+++ arch/arm/mach-ux500/include/mach/id.h
@@ -64,6 +64,15 @@ static inline bool __attribute_const__ cpu_is_u5500(void)
 #endif
 }
 
+#ifdef CONFIG_UX500_SOC_DB8500
+inline bool cpu_is_u9500(void);
+#else
+static inline bool cpu_is_u9500(void)
+{
+	return false;
+}
+#endif
+
 /*
  * 5500 revisions
  */
