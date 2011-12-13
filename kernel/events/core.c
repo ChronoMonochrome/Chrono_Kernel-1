640489eca01ea175d09ad41f8f9a283bfb42f8fa
Tejun Heo <tj@kernel.org>
2011-12-12 18:12:21 -0800

    cgroup: don't use subsys->can_attach_task() or ->attach_task()
    Now that subsys->can_attach() and attach() take @tset instead of
    @task, they can handle per-task operations.  Convert
    ->can_attach_task() and ->attach_task() users to use ->can_attach()
    and attach() instead.  Most converions are straight-forward.
    Noteworthy changes are,
    
    * In cgroup_freezer, remove unnecessary NULL assignments to unused
      methods.  It's useless and very prone to get out of sync, which
      already happened.
    
    * In cpuset, PF_THREAD_BOUND test is checked for each task.  This
      doesn't make any practical difference but is conceptually cleaner.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Reviewed-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Paul Menage <paul@paulmenage.org>
    Cc: Balbir Singh <bsingharora@gmail.com>
    Cc: Daisuke Nishimura <nishimura@mxp.nes.nec.co.jp>
    Cc: James Morris <jmorris@namei.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Peter Zijlstra <peterz@infradead.org>
    
    Conflicts:
    	block/blk-cgroup.c
    	kernel/sched.c

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7179,10 +7179,13 @@ static int __perf_cgroup_move(void *info)
 	return 0;
 }
 
-static void
-perf_cgroup_attach_task(struct cgroup *cgrp, struct task_struct *task)
+static void perf_cgroup_attach(struct cgroup_subsys *ss, struct cgroup *cgrp,
+			       struct cgroup_taskset *tset)
 {
-	task_function_call(task, __perf_cgroup_move, task);
+	struct task_struct *task;
+
+	cgroup_taskset_for_each(task, cgrp, tset)
+		task_function_call(task, __perf_cgroup_move, task);
 }
 
 static void perf_cgroup_exit(struct cgroup_subsys *ss, struct cgroup *cgrp,
@@ -7196,7 +7199,7 @@ static void perf_cgroup_exit(struct cgroup_subsys *ss, struct cgroup *cgrp,
 	if (!(task->flags & PF_EXITING))
 		return;
 
-	perf_cgroup_attach_task(cgrp, task);
+	task_function_call(task, __perf_cgroup_move, task);
 }
 
 struct cgroup_subsys perf_subsys = {
@@ -7205,6 +7208,6 @@ struct cgroup_subsys perf_subsys = {
 	.create		= perf_cgroup_create,
 	.destroy	= perf_cgroup_destroy,
 	.exit		= perf_cgroup_exit,
-	.attach_task	= perf_cgroup_attach_task,
+	.attach		= perf_cgroup_attach,
 };
 #endif /* CONFIG_CGROUP_PERF */
