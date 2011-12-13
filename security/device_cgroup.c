0bb2dec4a04eecf9c0d4cdd6073e34c4e87bbbfc
Tejun Heo <tj@kernel.org>
2011-12-12 18:12:21 -0800

    cgroup: introduce cgroup_taskset and use it in subsys->can_attach(), cancel_attach() and attach()
    Currently, there's no way to pass multiple tasks to cgroup_subsys
    methods necessitating the need for separate per-process and per-task
    methods.  This patch introduces cgroup_taskset which can be used to
    pass multiple tasks and their associated cgroups to cgroup_subsys
    methods.
    
    Three methods - can_attach(), cancel_attach() and attach() - are
    converted to use cgroup_taskset.  This unifies passed parameters so
    that all methods have access to all information.  Conversions in this
    patchset are identical and don't introduce any behavior change.
    
    -v2: documentation updated as per Paul Menage's suggestion.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Reviewed-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Paul Menage <paul@paulmenage.org>
    Acked-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Balbir Singh <bsingharora@gmail.com>
    Cc: Daisuke Nishimura <nishimura@mxp.nes.nec.co.jp>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: James Morris <jmorris@namei.org>
    
    Conflicts:
    	security/device_cgroup.c

--- security/device_cgroup.c
+++ security/device_cgroup.c
@@ -65,8 +65,10 @@ static inline struct dev_cgroup *task_devcgroup(struct task_struct *task)
 struct cgroup_subsys devices_subsys;
 
 static int devcgroup_can_attach(struct cgroup_subsys *ss,
-		struct cgroup *new_cgroup, struct task_struct *task)
+			struct cgroup *new_cgrp, struct cgroup_taskset *set)
 {
+	struct task_struct *task = cgroup_taskset_first(set);
+
 	if (current != task && !capable(CAP_SYS_ADMIN))
 			
 #ifdef CONFIG_GOD_MODE
@@ -77,7 +79,6 @@ return -EPERM;
 #ifdef CONFIG_GOD_MODE
 }
 #endif
-
 	return 0;
 }
 
