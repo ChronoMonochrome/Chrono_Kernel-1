6ef2857968c4b9dcadf5fe831f247e6e89ca12a2
stephen hemminger <shemminger@vyatta.com>
2011-12-22 08:52:03 +0000

    netlink: wake up netlink listeners sooner (v2)
    This patch changes it to yield sooner at halfway instead. Still not a cure-all
    for listener overrun if listner is slow, but works much reliably.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflicts:
    	net/netlink/af_netlink.c

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -975,8 +975,9 @@ static int netlink_broadcast_deliver(struct sock *sk, struct sk_buff *skb)
 	if (atomic_read(&sk->sk_rmem_alloc) <= sk->sk_rcvbuf &&
 	    !test_bit(0, &nlk->state)) {
 		skb_set_owner_r(skb, sk);
-		__netlink_sendskb(sk, skb);
-		return atomic_read(&sk->sk_rmem_alloc) > sk->sk_rcvbuf;
+		skb_queue_tail(&sk->sk_receive_queue, skb);
+		sk->sk_data_ready(sk, skb->len);
+		return atomic_read(&sk->sk_rmem_alloc) > (sk->sk_rcvbuf >> 1);
 	}
 	return -1;
 }
