1138a0bd24e6303160e8a16362f17d2fcc3da87a
Philippe Langlais <philippe.langlais@linaro.org>
2012-01-11 14:18:27 +0100

    mach-ux500: mcde: Remove static board configuration
    Make device registration more dynamic using mach_is_xxx()
    and uib_is_xxx() type of calls instead of having compile
    time Kconfig settings. And clean out most unused static
    configuration data.
    
    ST-Ericsson ID: 368402
    ST-Ericsson FOSS-OUT ID: Trivial
    ST-Ericsson Linux next: NA
    
    Signed-off-by: Marcus Lorentzon <marcus.xm.lorentzon@stericsson.com>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -788,7 +788,6 @@ static struct led_pwm pwm_leds_data[] = {
 		.lth_brightness = 90,
 		.pwm_period_ns = 1023,
 	},
-#ifdef CONFIG_DISPLAY_GENERIC_DSI_SECONDARY
 	[1] = {
 		.name = "sec-lcd-backlight",
 		.pwm_id = 2,
@@ -796,15 +795,10 @@ static struct led_pwm pwm_leds_data[] = {
 		.lth_brightness = 90,
 		.pwm_period_ns = 1023,
 	},
-#endif
 };
 
 static struct led_pwm_platform_data u8500_leds_data = {
-#ifdef CONFIG_DISPLAY_GENERIC_DSI_SECONDARY
-	.num_leds = 2,
-#else
 	.num_leds = 1,
-#endif
 	.leds = pwm_leds_data,
 };
 
@@ -1361,6 +1355,8 @@ static void __init hrefv60_init_machine(void)
 	hsi_register_board_info(u8500_hsi_devices,
 				ARRAY_SIZE(u8500_hsi_devices));
 #endif
+	if (uib_is_stuib())
+		u8500_leds_data.num_leds = 2;
 
 	mop500_sensors1p_init();
 
