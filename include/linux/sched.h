67e6939303fa4d4e766aec349670e4aee4bfbeee
Randy Dunlap <rdunlap@xenotime.net>
2012-01-21 11:03:13 -0800

    kernel-doc: fix kernel-doc warnings in sched
    Fix new kernel-doc notation warnings:
    
    Warning(include/linux/sched.h:2094): No description found for parameter 'p'
    Warning(include/linux/sched.h:2094): Excess function parameter 'tsk' description in 'is_idle_task'
    Warning(kernel/sched/cpupri.c:139): No description found for parameter 'newpri'
    Warning(kernel/sched/cpupri.c:139): Excess function parameter 'pri' description in 'cpupri_set'
    Warning(kernel/sched/cpupri.c:208): Excess function parameter 'bootmem' description in 'cpupri_init'
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Cc:	Ingo Molnar <mingo@elte.hu>
    Cc:	Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2094,7 +2094,7 @@ extern int sched_setscheduler_nocheck(struct task_struct *, int,
 extern struct task_struct *idle_task(int cpu);
 /**
  * is_idle_task - is the specified task an idle task?
- * @tsk: the task in question.
+ * @p: the task in question.
  */
 static inline bool is_idle_task(const struct task_struct *p)
 {
