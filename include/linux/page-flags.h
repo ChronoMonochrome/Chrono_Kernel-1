d0c2d3ea48caaa0d69a66c04f5cc0fc3ac992d56
Dan Magenheimer <dan.magenheimer@oracle.com>
2012-01-25 16:58:46 -0800

    mm: implement WasActive page flag (for improving cleancache)
    (Feedback welcome if there is a different/better way to do this
    without using a page flag!)
    
    Since about 2.6.27, the page replacement algorithm maintains
    an "active" bit to help decide which pages are most eligible
    to reclaim, see http://linux-mm.org/PageReplacementDesign
    
    This "active' information is also useful to cleancache but is lost
    by the time that cleancache has the opportunity to preserve the
    pageful of data.  This patch adds a new page flag "WasActive" to
    retain the state.  The flag may possibly be useful elsewhere.
    
    It is up to each cleancache backend to utilize the bit as
    it desires.  The matching patch for zcache is included here
    for clarification/discussion purposes, though it will need to
    go through GregKH and the staging tree.
    
    The patch resolves issues reported with cleancache which occur
    especially during streaming workloads on older processors,
    see https://lkml.org/lkml/2011/8/17/351
    
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    
    Conflicts:
    	include/linux/page-flags.h
    
    Conflicts:
    	include/linux/page-flags.h

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -51,6 +51,9 @@
  * PG_hwpoison indicates that a page got corrupted in hardware and contains
  * data with incorrect ECC bits that triggered a machine check. Accessing is
  * not safe since it may cause another machine check. Don't touch!
+ *
+ * PG_wasactive reflects that a page previously was promoted to active status.
+ * Such pages should be considered higher priority for cleancache backends.
  */
 
 /*
@@ -109,6 +112,9 @@ enum pageflags {
 #endif
 #ifdef CONFIG_KSM_CHECK_PAGE
 	PG_ksm_scan0,           /* page has been scanned by even KSM cycle */
+#endif
+#ifdef CONFIG_CLEANCACHE
+	PG_was_active,
 #endif
 	__NR_PAGEFLAGS,
 
@@ -224,6 +230,9 @@ CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
 #endif
 
 __PAGEFLAG(SlubFrozen, slub_frozen)
+#ifdef CONFIG_CLEANCACHE
+PAGEFLAG(WasActive, was_active)
+#endif
 
 /*
  * Private page markings that may be used by the filesystem that owns the page
