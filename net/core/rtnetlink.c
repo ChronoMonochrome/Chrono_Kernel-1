e22388ec9da4a5e456a45ce3286a7c0675f6c794
Stefan Gula <steweg@gmail.com>
2012-01-26 11:01:06 +0000

    net: RTNETLINK adjusting values of min_ifinfo_dump_size
    Setting link parameters on a netdevice changes the value
    of if_nlmsg_size(), therefore it is necessary to recalculate
    min_ifinfo_dump_size.
    
    Signed-off-by: Stefan Gula <steweg@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -1506,6 +1506,9 @@ errout:
 
 	if (send_addr_notify)
 		call_netdevice_notifiers(NETDEV_CHANGEADDR, dev);
+	min_ifinfo_dump_size = max_t(u16, if_nlmsg_size(dev),
+				     min_ifinfo_dump_size);
+
 	return err;
 }
 
