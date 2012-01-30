23a5d9f8b47ecacb1d6e5f312f4be85bafc7b2b9
Jiri Slaby <jslaby@suse.cz>
2012-01-30 21:14:28 +0100

    TTY: provide tty_standard_install helper
    There are currently many cut&paste copies of what
    tty_driver_install_tty does when custom ->install method is not
    provided. Let's get rid of the copies and create a helper with this
    setup code.
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>
    Cc: Havard Skinnemoen <hskinnemoen@google.com>
    Acked-by: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	include/linux/tty.h

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1337,21 +1337,8 @@ EXPORT_SYMBOL_GPL(tty_standard_install);
 static int tty_driver_install_tty(struct tty_driver *driver,
 						struct tty_struct *tty)
 {
-	int idx = tty->index;
-	int ret;
-
-	if (driver->ops->install) {
-		ret = driver->ops->install(driver, tty);
-		return ret;
-	}
-
-	if (tty_init_termios(tty) == 0) {
-		tty_driver_kref_get(driver);
-		tty->count++;
-		driver->ttys[idx] = tty;
-		return 0;
-	}
-	return -ENOMEM;
+	return driver->ops->install ? driver->ops->install(driver, tty) :
+		tty_standard_install(driver, tty);
 }
 
 /**
