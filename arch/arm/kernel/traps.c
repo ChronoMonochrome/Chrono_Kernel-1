fdc089a3a99cffdc3735faff3522d53aeadfd9ca
Russell King <rmk+kernel@arm.linux.org.uk>
2012-02-04 12:12:11 +0000

    ARM: Add compiled ISA to oops dumps
    Add the compiled ISA to oops dumps, along side the preempt/smp
    configuration.  This allows us to see immediately whether the kernel
    was compiled for Thumb-2 or not.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -269,6 +269,11 @@ void show_stack(struct task_struct *tsk, unsigned long *sp)
 #else
 #define S_SMP ""
 #endif
+#ifdef CONFIG_THUMB2_KERNEL
+#define S_ISA " THUMB2"
+#else
+#define S_ISA " ARM"
+#endif
 
 static int __die(const char *str, int err, struct thread_info *thread, struct pt_regs *regs)
 {
@@ -276,12 +281,8 @@ static int __die(const char *str, int err, struct thread_info *thread, struct pt
 	static int die_counter;
 	int ret;
 
-#ifdef CONFIG_DEBUG_PRINTK
-	printk(KERN_EMERG "Internal error: %s: %x [#%d]" S_PREEMPT S_SMP "\n",
-	       str, err, ++die_counter);
-#else
-	;
-#endif
+	printk(KERN_EMERG "Internal error: %s: %x [#%d]" S_PREEMPT S_SMP
+	       S_ISA "\n", str, err, ++die_counter);
 
 	/* trap and error numbers are mostly meaningless on ARM */
 	ret = notify_die(DIE_OOPS, str, regs, err, tsk->thread.trap_no, SIGSEGV);
