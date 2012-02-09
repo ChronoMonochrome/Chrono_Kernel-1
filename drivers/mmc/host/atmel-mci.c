673b25f0057d5851b8771da06f057e2754cf7e90
Ludovic Desroches <ludovic.desroches@atmel.com>
2012-02-09 16:33:54 +0100

    mmc: atmel-mci: fix typo
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/atmel-mci.c
+++ drivers/mmc/host/atmel-mci.c
@@ -1970,7 +1970,7 @@ static void atmci_configure_dma(struct atmel_mci *host)
 		dev_notice(&host->pdev->dev, "DMA not available, using PIO\n");
 	} else {
 		dev_info(&host->pdev->dev,
-					"Using %s for DMA transfers\n",
+					"using %s for DMA transfers\n",
 					dma_chan_name(host->dma.chan));
 
 		host->dma_conf.src_addr = host->mapbase + ATMCI_RDR;
