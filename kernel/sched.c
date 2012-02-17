de9733c38205a98d66d09d4cbe8700715cf1cb57
faux123 <reioux@gmail.com>
2012-02-16 22:01:32 -0800

    sched: Make initial SCHED_RR timeslace DEF_TIMESLICE
    From: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    
    Current the initial SCHED_RR timeslice of init_task is HZ, which means
    1s, and is not same as the default SCHED_RR timeslice DEF_TIMESLICE.
    
    Change that initial timeslice to the DEF_TIMESLICE.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    adapted by faux123 for linux 3.0.x

--- kernel/sched.c
+++ kernel/sched.c
@@ -118,11 +118,7 @@ ATOMIC_NOTIFIER_HEAD(migration_notifier_head);
 
 /*
  * These are the 'tuning knobs' of the scheduler:
- *
- * default timeslice is 100 msecs (used only for SCHED_RR tasks).
- * Timeslices get refilled after they expire.
  */
-#define DEF_TIMESLICE		(100 * HZ / 1000)
 
 /*
  * single value that denotes runtime == period, ie unlimited time.
