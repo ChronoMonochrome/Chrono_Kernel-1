004eb1db89e61c558d0d8f50e46e21d1aaaea36e
Ulf Hansson <ulf.hansson@stericsson.com>
2012-02-22 10:57:24 +0100

    ARM: u8500: board-sdi: Remove not needed ocr_mask
    For sdi0 and sdi4 there are external regulators connected
    to the cards ("vmmc"). These will be used to find out the
    supported ocr_masks.
    
    Change-Id: I67b80bc14c78a45866aa4b2d5f07903d2ee414d3
    Signed-off-by: Ulf Hansson <ulf.hansson@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/49937
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    Reviewed-by: QABUILD

--- arch/arm/mach-ux500/board-mop500-sdi.c
+++ arch/arm/mach-ux500/board-mop500-sdi.c
@@ -95,7 +95,6 @@ static struct stedma40_chan_cfg mop500_sdi0_dma_cfg_tx = {
 
 static struct mmci_platform_data mop500_sdi0_data = {
 	.ios_handler	= mop500_sdi0_ios_handler,
-	.ocr_mask	= MMC_VDD_29_30,
 	.f_max		= 50000000,
 	.capabilities	= MMC_CAP_4_BIT_DATA |
 				MMC_CAP_SD_HIGHSPEED |
@@ -234,7 +233,6 @@ static struct stedma40_chan_cfg mop500_sdi4_dma_cfg_tx = {
 #endif
 
 static struct mmci_platform_data mop500_sdi4_data = {
-	.ocr_mask	= MMC_VDD_29_30,
 	.f_max		= 50000000,
 	.capabilities	= MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA |
 			  MMC_CAP_MMC_HIGHSPEED,
