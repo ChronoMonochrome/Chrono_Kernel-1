53223686faa189a61e71d1153671962ffb4c34ca
Jonas Aaberg <jonas.aberg@stericsson.com>
2012-02-27 11:02:44 +0100

    power: ab8500_btemp: Detect battery type in workqueue
    Detect battery type in work queue instead of probe.
    This reduces the system boot time with 1.5s
    
    ST-Ericsson Linux next: -
    ST-Ericsson ID: 370799
    ST-Ericsson FOSS-OUT ID: Trivial
    
    Change-Id: I6d21df2746a8a08940e18e793ec0e171eda67708
    Signed-off-by: Jonas Aaberg <jonas.aberg@stericsson.com>
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/50551
    Reviewed-by: QABUILD
    Reviewed-by: Karl KOMIEROWSKI <karl.komierowski@stericsson.com>

--- drivers/power/ab8500_btemp.c
+++ drivers/power/ab8500_btemp.c
@@ -83,6 +83,7 @@ struct ab8500_btemp_ranges {
  * @btemp_ranges:	Battery temperature range structure
  * @btemp_wq:		Work queue for measuring the temperature periodically
  * @btemp_periodic_work:	Work for measuring the temperature periodically
+ * @initialized:	True if battery id read.
  */
 struct ab8500_btemp {
 	struct device *dev;
@@ -100,6 +101,7 @@ struct ab8500_btemp {
 	struct ab8500_btemp_ranges btemp_ranges;
 	struct workqueue_struct *btemp_wq;
 	struct delayed_work btemp_periodic_work;
+	bool initialized;
 };
 
 /* BTEMP power supply properties */
@@ -569,6 +571,13 @@ static void ab8500_btemp_periodic_work(struct work_struct *work)
 	struct ab8500_btemp *di = container_of(work,
 		struct ab8500_btemp, btemp_periodic_work.work);
 
+	if (!di->initialized) {
+		di->initialized = true;
+		/* Identify the battery */
+		if (ab8500_btemp_id(di) < 0)
+			dev_warn(di->dev, "failed to identify the battery\n");
+	}
+
 	di->bat_temp = ab8500_btemp_measure_temp(di);
 
 	if (di->bat_temp != di->prev_bat_temp) {
@@ -976,6 +985,8 @@ static int __devinit ab8500_btemp_probe(struct platform_device *pdev)
 	di->parent = dev_get_drvdata(pdev->dev.parent);
 	di->gpadc = ab8500_gpadc_get("ab8500-gpadc.0");
 
+	di->initialized = false;
+
 	/* get btemp specific platform data */
 	plat_data = pdev->dev.platform_data;
 	di->pdata = plat_data->btemp;
@@ -1016,10 +1027,6 @@ static int __devinit ab8500_btemp_probe(struct platform_device *pdev)
 	/* Init work for measuring temperature periodically */
 	INIT_DELAYED_WORK_DEFERRABLE(&di->btemp_periodic_work,
 		ab8500_btemp_periodic_work);
-
-	/* Identify the battery */
-	if (ab8500_btemp_id(di) < 0)
-		dev_warn(di->dev, "failed to identify the battery\n");
 
 	/* Set BTEMP thermal limits. Low and Med are fixed */
 	di->btemp_ranges.btemp_low_limit = BTEMP_THERMAL_LOW_LIMIT;
