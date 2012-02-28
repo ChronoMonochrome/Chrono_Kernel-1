db59ae830656e3c87f0ace7edbade0a684ca2c7d
Philippe Langlais <philippe.langlais@linaro.org>
2012-02-28 16:40:39 +0100

    ux500: rmi4: Align touch with display
    When hiding display physical orientation in previous patch
    touch gets out of sync with display. This patch realign touch
    with display.
    
    Signed-off-by: Marcus Lorentzon <marcus.xm.lorentzon@stericsson.com>

--- drivers/staging/ste_rmi4/board-mop500-u8500uib-rmi4.c
+++ drivers/staging/ste_rmi4/board-mop500-u8500uib-rmi4.c
@@ -20,8 +20,8 @@
 static struct synaptics_rmi4_platform_data rmi4_i2c_dev_platformdata = {
 	.irq_number     = NOMADIK_GPIO_TO_IRQ(84),
 	.irq_type       = (IRQF_TRIGGER_FALLING | IRQF_SHARED),
-	.x_flip		= false,
-	.y_flip		= true,
+	.x_flip		= true,
+	.y_flip		= false,
 	.regulator_en   = true,
 };
 
