1828073b743cba36d2fa9951e5d0ec1fd9dea61f
Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>
2012-02-29 18:00:21 +0530

    ux500: input:misc:accdet: Add ab8505 accessory detect support
    Add the new mic control gpio found in the 8520 board.
    Gpio is used to configure the mux, so that the 3.5mm
    jack gets connected to either the audio or video part
    of the board.
    
    ST-Ericsson ID: 366316
    
    Signed-off-by: Virupax Sadashivpetimath <virupax.sadashivpetimath@stericsson.com>

--- arch/arm/mach-ux500/board-mop500.c
+++ arch/arm/mach-ux500/board-mop500.c
@@ -185,7 +185,7 @@ static struct ab8500_gpio_platform_data ab8505_gpio_pdata = {
 	 * as GPIO then this register selectes the alternate functions
 	 * GpioSel7 = 0x22 => Pins GPIO50 to GPIO52 are configured as GPIO.
 	 */
-	.config_reg     = {0x0F, 0x9E, 0x80, 0x01, 0x78, 0x02, 0x22},
+	.config_reg     = {0x0F, 0x9E, 0x80, 0x01, 0x7A, 0x02, 0x22},
 
 	/*
 	 * config_direction allows for the initial GPIO direction to
@@ -1319,6 +1319,11 @@ static void __init hrefv60_init_machine(void)
 	 *  button press/release are inverted.
 	 */
 	ab8500_accdet_pdata.is_detection_inverted = true;
+
+	if (machine_is_u8520()) {
+		ab8500_accdet_pdata.video_ctrl_gpio = AB8500_PIN_GPIO10;
+		ab8500_accdet_pdata.mic_ctrl = AB8500_PIN_GPIO34;
+	}
 #endif
 
 	parent = u8500_init_devices();
