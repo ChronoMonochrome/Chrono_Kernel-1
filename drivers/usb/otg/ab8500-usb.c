igloocommunity/igloo-kernel

USB: Add support for USB OTG 2.0
OTG 2.0 support is provided in Kernel 3.0.
Srp related modifications are done in ab8500-usb.

ST-Ericsson ID: 401192
ST-Ericsson Linux next: NA
ST-Ericsson FOSS-OUT ID: STETL-FOSS-OUT-10054

Change-Id: I1bf52c8d6f6c4b0bedf5e51004dc72bf52a68020
Signed-off-by: Avinash Kumar <avinash.kumar@stericsson.com>
Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/49006
Reviewed-by: QATOOLS
Reviewed-by: Praveena NADAHALLY <praveen.nadahally@stericsson.com>

Conflicts:

	drivers/usb/otg/ab8500-usb.c

drivers/usb/otg/ab8500-usb.c
@@ -484,6 +484,25 @@ static unsigned ab8500_eyediagram_workaroud(struct ab8500_usb *ab, unsigned mA)
 	return mA;
 }
 
+#ifdef CONFIG_USB_OTG_20
+static int ab8500_usb_start_srp(struct otg_transceiver *otg, unsigned mA)
+{
+	struct ab8500_usb *ab;
+	if (!otg)
+		return -ENODEV;
+
+	ab = xceiv_to_ab(otg);
+
+	atomic_notifier_call_chain(&ab->otg.notifier,
+				   USB_EVENT_PREPARE,
+				   &ab->vbus_draw);
+
+	ab8500_usb_peri_phy_en(ab);
+
+	return 0;
+}
+#endif
+
 static int ab8500_usb_set_power(struct otg_transceiver *otg, unsigned mA)
 {
 	struct ab8500_usb *ab;
@@ -825,6 +844,9 @@ static int __devinit ab8500_usb_probe(struct platform_device *pdev)
 	ab->otg.set_peripheral	= ab8500_usb_set_peripheral;
 	ab->otg.set_suspend	= ab8500_usb_set_suspend;
 	ab->otg.set_power	= ab8500_usb_set_power;
+#ifdef CONFIG_USB_OTG_20
+	ab->otg.start_srp	= ab8500_usb_start_srp;
+#endif
 	ab->sysfs_flag = true;
 
 	platform_set_drvdata(pdev, ab);

ChronoMonochrome/Chrono_Kernel-1

drivers/usb/otg/ab8500-usb.c
@@ -484,6 +484,26 @@ static unsigned ab8500_eyediagram_workaroud(struct ab8500_usb *ab, unsigned mA)
 	return mA;
 }
 
+#ifdef CONFIG_USB_OTG_20
+static int ab8500_usb_start_srp(struct usb_phy *phy, unsigned mA)
+{
+	struct ab8500_usb *ab;
+
+	if (!phy)
+		return -ENODEV;
+
+	ab = phy_to_ab(phy);
+
+	atomic_notifier_call_chain(&ab->phy.notifier,
+				   USB_EVENT_PREPARE,
+				   &ab->vbus_draw);
+
+	ab8500_usb_peri_phy_en(ab);
+
+	return 0;
+}
+#endif
+
 static int ab8500_usb_set_power(struct usb_phy *phy, unsigned mA)
 {
 	struct ab8500_usb *ab;
@@ -838,6 +858,9 @@ static int __devinit ab8500_usb_probe(struct platform_device *pdev)
 	otg->phy		= &ab->phy;
 	otg->set_host		= ab8500_usb_set_host;
 	otg->set_peripheral	= ab8500_usb_set_peripheral;
+#ifdef CONFIG_USB_OTG_20
+	ab->otg.start_srp	= ab8500_usb_start_srp;
+#endif
 	ab->sysfs_flag		= true;
 
 	platform_set_drvdata(pdev, ab);