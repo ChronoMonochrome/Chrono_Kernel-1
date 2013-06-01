5a200af43a8fff4ef445ddf76a26c523ee930ed3
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-06-01 13:33:20 +0200

    mm: Dynamic management of dirty page writebacks
    This feature allows to have two different intervals for dirty page
    writebacks and to dynamically manage them when the system has been
    resumed (it's active) or when has been suspended.
    
    Three new procfs parameters are exposed inside /proc/sys/vm path:
    - "dynamic_dirty_writeback" is the activation status of this feature,
      set 1 to enable it, set 0 to disable it and use the standard behaviour
    - "dirty_writeback_active_centisecs" is the interval for the dirty page
      writebacks when the system is active (screen on)
    - "dirty_writeback_suspend_centisecs" is the interval for the dirty page
      writebacks when the system is suspended (screen off)
    
    This feature is in part inspired on Francisco Franco's patch:
    https://github.com/franciscofranco/mako/commit/34d79548cf16b69c7d0154836018e7f258a22947
    
    Signed-off-by: Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
    
    Conflicts:
    	kernel/sysctl.c
    	mm/Kconfig

--- mm/backing-dev.c
+++ mm/backing-dev.c
@@ -225,7 +225,9 @@ static ssize_t max_ratio_store(struct device *dev,
 }
 BDI_SHOW(max_ratio, bdi->max_ratio)
 
+#ifndef __ATTR_RW
 #define __ATTR_RW(attr) __ATTR(attr, 0644, attr##_show, attr##_store)
+#endif
 
 static struct device_attribute bdi_dev_attrs[] = {
 	__ATTR_RW(read_ahead_kb),
