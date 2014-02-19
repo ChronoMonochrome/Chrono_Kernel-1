1cc258af1d00e83a00c58a9cbce75b65d2e56075
Ruchi Kandoi <kandoiruchi@google.com>
2014-02-19 15:30:47 -0800

    Power: add an API to log wakeup reasons
    Add API log_wakeup_reason() and expose it to userspace via sysfs path
    /sys/kernel/wakeup_reasons/last_resume_reason
    
    Change-Id: I81addaf420f1338255c5d0638b0d244a99d777d1
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    
    Conflicts:
    	kernel/power/Makefile
    	kernel/power/wakeup_reason.c

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -34,8 +34,8 @@ static int irq_count;
 static struct kobject *wakeup_reason;
 static spinlock_t resume_reason_lock;
 
-static ssize_t last_resume_reason_show(struct kobject *kobj, struct kobj_attribute *attr,
-		char *buf)
+static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
+		const char *buf, size_t count)
 {
 	int irq_no, buf_offset = 0;
 	struct irq_desc *desc;
@@ -53,7 +53,8 @@ static ssize_t last_resume_reason_show(struct kobject *kobj, struct kobj_attribu
 	return buf_offset;
 }
 
-static struct kobj_attribute resume_reason = __ATTR_RO(last_resume_reason);
+static struct kobj_attribute resume_reason = __ATTR(last_resume_reason, 0666,
+		reason_show, NULL);
 
 static struct attribute *attrs[] = {
 	&resume_reason.attr,
@@ -72,31 +73,12 @@ void log_wakeup_reason(int irq)
 	struct irq_desc *desc;
 	desc = irq_to_desc(irq);
 	if (desc && desc->action && desc->action->name)
-#ifdef CONFIG_DEBUG_PRINTK
 		printk(KERN_INFO "Resume caused by IRQ %d, %s\n", irq,
 				desc->action->name);
-#else
-		;
-#endif
 	else
-#ifdef CONFIG_DEBUG_PRINTK
 		printk(KERN_INFO "Resume caused by IRQ %d\n", irq);
-#else
-		;
-#endif
 
 	spin_lock(&resume_reason_lock);
-	if (irq_count == MAX_WAKEUP_REASON_IRQS) {
-		spin_unlock(&resume_reason_lock);
-#ifdef CONFIG_DEBUG_PRINTK
-		printk(KERN_WARNING "Resume caused by more than %d IRQs\n",
-				MAX_WAKEUP_REASON_IRQS);
-#else
-		;
-#endif
-		return;
-	}
-
 	irq_list[irq_count++] = irq;
 	spin_unlock(&resume_reason_lock);
 }
@@ -124,40 +106,27 @@ static struct notifier_block wakeup_reason_pm_notifier_block = {
 /* Initializes the sysfs parameter
  * registers the pm_event notifier
  */
-int __init wakeup_reason_init(void)
+void __init wakeup_reason_init(void)
 {
 	int retval;
 	spin_lock_init(&resume_reason_lock);
 	retval = register_pm_notifier(&wakeup_reason_pm_notifier_block);
 	if (retval)
-#ifdef CONFIG_DEBUG_PRINTK
 		printk(KERN_WARNING "[%s] failed to register PM notifier %d\n",
 				__func__, retval);
-#else
-		;
-#endif
 
 	wakeup_reason = kobject_create_and_add("wakeup_reasons", kernel_kobj);
 	if (!wakeup_reason) {
-#ifdef CONFIG_DEBUG_PRINTK
 		printk(KERN_WARNING "[%s] failed to create a sysfs kobject\n",
 				__func__);
-#else
-		;
-#endif
-		return 1;
+		return;
 	}
 	retval = sysfs_create_group(wakeup_reason, &attr_group);
 	if (retval) {
 		kobject_put(wakeup_reason);
-#ifdef CONFIG_DEBUG_PRINTK
 		printk(KERN_WARNING "[%s] failed to create a sysfs group %d\n",
 				__func__, retval);
-#else
-		;
-#endif
 	}
-	return 0;
 }
 
 late_initcall(wakeup_reason_init);
