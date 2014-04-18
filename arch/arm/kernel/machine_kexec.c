ff0fb54c4a97cce9cdde1d33cea3f56f76781462
Liu Hua <sdu.liu@huawei.com>
2014-04-18 07:45:36 +0100

    ARM: 8030/1: ARM : kdump : add arch_crash_save_vmcoreinfo
    commit 56b700fd6f1e49149880fb1b6ffee0dca5be45fb upstream.
    
    For vmcore generated by LPAE enabled kernel, user space
    utility such as crash needs additional infomation to
    parse.
    
    So this patch add arch_crash_save_vmcoreinfo as what PAE enabled
    i386 linux does.
    
    Reviewed-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Liu Hua <sdu.liu@huawei.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kernel/machine_kexec.c
+++ arch/arm/kernel/machine_kexec.c
@@ -163,3 +163,10 @@ void machine_kexec(struct kimage *image)
 	 */
 	((typeof(cpu_reset) *)virt_to_phys(cpu_reset))(reboot_code_buffer_phys);
 }
+
+void arch_crash_save_vmcoreinfo(void)
+{
+#ifdef CONFIG_ARM_LPAE
+	VMCOREINFO_CONFIG(ARM_LPAE);
+#endif
+}
