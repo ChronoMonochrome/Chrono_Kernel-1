0c904bce298a3c36d8cf82959c9274256e6adafc
Chrono Monochrome <chrono.monochrome@gmail.com>
2014-09-28 13:46:40 +0400

    vfs, f2fs: use old variants of some fsync-related functions
    since we didn't apply commit "fs: push i_mutex and filemap_write_and_wait down into ->fsync()
     handlers" we just reverted this functions to LK 3.0

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -118,7 +118,7 @@ static int get_parent_ino(struct inode *inode, nid_t *pino)
 	return 1;
 }
 
-int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
+int f2fs_sync_file(struct file *file, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
 	struct f2fs_inode_info *fi = F2FS_I(inode);
@@ -135,16 +135,9 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		return 0;
 
 	trace_f2fs_sync_file_enter(inode);
-	ret = filemap_write_and_wait_range(inode->i_mapping, start, end);
-	if (ret) {
-		trace_f2fs_sync_file_exit(inode, need_cp, datasync, ret);
-		return ret;
-	}
 
 	/* guarantee free sections for fsync */
 	f2fs_balance_fs(sbi);
-	
-	mutex_lock(&inode->i_mutex);
 
 	down_read(&fi->i_sem);
 
@@ -201,11 +194,9 @@ int f2fs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		ret = f2fs_issue_flush(F2FS_SB(inode->i_sb));
 	}
 out:
-	mutex_unlock(&inode->i_mutex);
 	trace_f2fs_sync_file_exit(inode, need_cp, datasync, ret);
 	return ret;
 }
-
 static pgoff_t __get_first_dirty_index(struct address_space *mapping,
 						pgoff_t pgofs, int whence)
 {
