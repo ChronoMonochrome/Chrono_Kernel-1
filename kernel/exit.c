9b63680c46f6fd36b1b27a51bd861c0731dc8ca5
Shilin Victor <chrono.monochrome@gmail.com>
2014-10-13 05:48:34 +0400

    fix build errors after switch to refresh tree
    thanks @cocafe for fix for VR I/O sched
    
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -990,7 +990,6 @@ NORET_TYPE void do_exit(long code)
 	trace_sched_process_exit(tsk);
 
 	exit_sem(tsk);
-	exit_shm(tsk);
 	exit_files(tsk);
 	exit_fs(tsk);
 	check_stack_usage();
@@ -1384,8 +1383,7 @@ static int wait_task_zombie(struct wait_opts *wo, struct task_struct *p)
 static int *task_stopped_code(struct task_struct *p, bool ptrace)
 {
 	if (ptrace) {
-		if (task_is_stopped_or_traced(p) &&
-		    !(p->jobctl & JOBCTL_LISTENING))
+		if (task_is_stopped_or_traced(p))
 			return &p->exit_code;
 	} else {
 		if (p->signal->flags & SIGNAL_STOP_STOPPED)
