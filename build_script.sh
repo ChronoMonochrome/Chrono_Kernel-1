1eed8d73c4fdd81945511a8176abd3adcb7315e0
Shilin Victor <chrono.monochrome@gmail.com>
2015-06-05 14:17:45 +0300

    build_script: sync
    Conflicts:
    	fs/sysfs/inode.c

--- build_script.sh
+++ build_script.sh
@@ -2,7 +2,7 @@ SOURCE=~/kernel_build/chrono_kernel
 BUILD=~/kernel_build/obj
 
 clean(){
-    make O=$BUILD mkproper
+    make O=$BUILD mrproper
     make O=$BUILD clean
 }
 
@@ -63,7 +63,11 @@ inst(){
     if [ "$2" == "light" ] ; then 
         zip -r -9 $KERNEL_NAME META-INF system ramdisk boot.img tmp
     else
-        zip -r -9 $KERNEL_NAME META-INF system ramdisk genfstab osfiles recovery boot.img tmp
+        if [ "$2" == "light2" ] ; then
+             zip -r -9 $KERNEL_NAME META-INF system ramdisk genfstab boot.img tmp
+        else
+             zip -r -9 $KERNEL_NAME META-INF system ramdisk genfstab osfiles recovery boot.img tmp
+        fi
     fi
 
     cd $SOURCE
