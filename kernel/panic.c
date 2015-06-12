9e354689b272fcca5d06d6bd438e806d6ed6e454
Shilin Victor <chrono.monochrome@gmail.com>
2015-06-12 17:53:49 +0400

    kernel/panic.c: samsung changes

--- kernel/panic.c
+++ kernel/panic.c
@@ -23,6 +23,19 @@
 #include <linux/init.h>
 #include <linux/nmi.h>
 #include <linux/dmi.h>
+#ifdef CONFIG_SAMSUNG_KERNEL_DEBUG
+#include <asm/cacheflush.h>
+#include <linux/smp.h>
+#include <linux/percpu.h>
+
+/* L1 & L2 cache management */
+#include <asm/cacheflush.h>
+
+void ux500_clean_l2_cache_all(void);
+
+/* prcmu register dump */
+void dbx500_dump_in_panic(void);
+#endif /* CONFIG_SAMSUNG_KERNEL_DEBUG */
 
 #define PANIC_TIMER_STEP 100
 #define PANIC_BLINK_SPD 18
@@ -36,7 +49,10 @@ static int pause_on_oops;
 static int pause_on_oops_flag;
 static DEFINE_SPINLOCK(pause_on_oops_lock);
 
-int panic_timeout;
+#ifndef CONFIG_PANIC_TIMEOUT
+#define CONFIG_PANIC_TIMEOUT 0
+#endif
+int panic_timeout = CONFIG_PANIC_TIMEOUT;
 EXPORT_SYMBOL_GPL(panic_timeout);
 
 ATOMIC_NOTIFIER_HEAD(panic_notifier_list);
@@ -51,6 +67,290 @@ static long no_blink(int state)
 /* Returns how long it waited in ms */
 long (*panic_blink)(int state);
 EXPORT_SYMBOL(panic_blink);
+
+
+#ifdef CONFIG_SAMSUNG_KERNEL_DEBUG
+typedef struct tag_mmu_info {
+	int SCTLR;
+	int TTBR0;
+	int TTBR1;
+	int TTBCR;
+	int DACR;
+	int DFSR;
+	int DFAR;
+	int IFSR;
+	int IFAR;
+	int DAFSR;
+	int IAFSR;
+	int PMRRR;
+	int NMRRR;
+	int FCSEPID;
+	int CONTEXT;
+	int URWTPID;
+	int UROTPID;
+	int POTPIDR;
+} t_kernel_sec_mmu_info;
+
+/*ARM CORE regs mapping structure*/
+typedef struct {
+	/* COMMON */
+	unsigned int r0;
+	unsigned int r1;
+	unsigned int r2;
+	unsigned int r3;
+	unsigned int r4;
+	unsigned int r5;
+	unsigned int r6;
+	unsigned int r7;
+	unsigned int r8;
+	unsigned int r9;
+	unsigned int r10;
+	unsigned int r11;
+	unsigned int r12;
+
+	/* SVC */
+	unsigned int r13_svc;
+	unsigned int r14_svc;
+	unsigned int spsr_svc;
+
+	/* PC & CPSR */
+	unsigned int pc;
+	unsigned int cpsr;
+
+	/* USR/SYS */
+	unsigned int r13_usr;
+	unsigned int r14_usr;
+
+	/* FIQ */
+	unsigned int r8_fiq;
+	unsigned int r9_fiq;
+	unsigned int r10_fiq;
+	unsigned int r11_fiq;
+	unsigned int r12_fiq;
+	unsigned int r13_fiq;
+	unsigned int r14_fiq;
+	unsigned int spsr_fiq;
+
+	/* IRQ */
+	unsigned int r13_irq;
+	unsigned int r14_irq;
+	unsigned int spsr_irq;
+
+	/* MON */
+	unsigned int r13_mon;
+	unsigned int r14_mon;
+	unsigned int spsr_mon;
+
+	/* ABT */
+	unsigned int r13_abt;
+	unsigned int r14_abt;
+	unsigned int spsr_abt;
+
+	/* UNDEF */
+	unsigned int r13_und;
+	unsigned int r14_und;
+	unsigned int spsr_und;
+
+} t_kernel_sec_arm_core_regsiters;
+
+/*
+__used t_kernel_sec_arm_core_regsiters kernel_sec_core_reg_dump;
+__used t_kernel_sec_mmu_info kernel_sec_mmu_reg_dump;
+*/
+DEFINE_PER_CPU(t_kernel_sec_arm_core_regsiters, kernel_sec_core_reg_dump);
+DEFINE_PER_CPU(t_kernel_sec_mmu_info, kernel_sec_mmu_reg_dump);
+
+/* core reg dump function*/
+void kernel_sec_get_core_reg_dump(t_kernel_sec_arm_core_regsiters * regs)
+{
+	asm(
+		   // we will be in SVC mode when we enter this function. Collect SVC registers along with cmn registers.
+		   "str r0, [%0,#0] \n\t"	// R0
+		   "str r1, [%0,#4] \n\t"	// R1
+		   "str r2, [%0,#8] \n\t"	// R2
+		   "str r3, [%0,#12] \n\t"	// R3
+		   "str r4, [%0,#16] \n\t"	// R4
+		   "str r5, [%0,#20] \n\t"	// R5
+		   "str r6, [%0,#24] \n\t"	// R6
+		   "str r7, [%0,#28] \n\t"	// R7
+		   "str r8, [%0,#32] \n\t"	// R8
+		   "str r9, [%0,#36] \n\t"	// R9
+		   "str r10, [%0,#40] \n\t"	// R10
+		   "str r11, [%0,#44] \n\t"	// R11
+		   "str r12, [%0,#48] \n\t"	// R12
+
+		   /* SVC */
+		   "str r13, [%0,#52] \n\t"	// R13_SVC
+		   "str r14, [%0,#56] \n\t"	// R14_SVC
+		   "mrs r1, spsr \n\t"		// SPSR_SVC
+		   "str r1, [%0,#60] \n\t"
+
+		   /* PC and CPSR */
+		   "sub r1, r15, #0x4 \n\t"	// PC
+		   "str r1, [%0,#64] \n\t"
+		   "mrs r1, cpsr \n\t"		// CPSR
+		   "str r1, [%0,#68] \n\t"
+
+		   /* SYS/USR */
+		   "mrs r1, cpsr \n\t"		// switch to SYS mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x1f \n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r13, [%0,#72] \n\t"	// R13_USR
+		   "str r14, [%0,#76] \n\t"	// R13_USR
+
+		    /*FIQ*/
+		    "mrs r1, cpsr \n\t"		// switch to FIQ mode
+		   "and r1,r1,#0xFFFFFFE0\n\t"
+		   "orr r1,r1,#0x11\n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r8, [%0,#80] \n\t"	// R8_FIQ
+		   "str r9, [%0,#84] \n\t"	// R9_FIQ
+		   "str r10, [%0,#88] \n\t"	// R10_FIQ
+		   "str r11, [%0,#92] \n\t"	// R11_FIQ
+		   "str r12, [%0,#96] \n\t"	// R12_FIQ
+		   "str r13, [%0,#100] \n\t"	// R13_FIQ
+		   "str r14, [%0,#104] \n\t"	// R14_FIQ
+		   "mrs r1, spsr \n\t"		// SPSR_FIQ
+		   "str r1, [%0,#108] \n\t"
+
+		   /*IRQ*/
+		   "mrs r1, cpsr \n\t"		// switch to IRQ mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x12\n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r13, [%0,#112] \n\t"	// R13_IRQ
+		   "str r14, [%0,#116] \n\t"	// R14_IRQ
+		   "mrs r1, spsr \n\t"		// SPSR_IRQ
+		   "str r1, [%0,#120] \n\t"
+
+		    /*MON*/
+		   "mrs r1, cpsr \n\t"		// switch to monitor mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x16\n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r13, [%0,#124] \n\t"	// R13_MON
+		   "str r14, [%0,#128] \n\t"	// R14_MON
+		   "mrs r1, spsr \n\t"		// SPSR_MON
+		   "str r1, [%0,#132] \n\t"
+
+		   /*ABT*/
+		   "mrs r1, cpsr \n\t"		// switch to Abort mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x17\n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r13, [%0,#136] \n\t"	// R13_ABT
+		   "str r14, [%0,#140] \n\t"	// R14_ABT
+		   "mrs r1, spsr \n\t"		// SPSR_ABT
+		   "str r1, [%0,#144] \n\t"
+		   
+		   /*UND*/
+		   "mrs r1, cpsr \n\t"		// switch to undef mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x1B\n\t"
+		   "msr cpsr,r1 \n\t"
+
+		   "str r13, [%0,#148] \n\t"	// R13_UND
+		   "str r14, [%0,#152] \n\t"	// R14_UND
+		   "mrs r1, spsr \n\t"		// SPSR_UND
+		   "str r1, [%0,#156] \n\t"
+
+		   /* restore to SVC mode */
+		   "mrs r1, cpsr \n\t"		// switch to undef mode
+		   "and r1, r1, #0xFFFFFFE0\n\t"
+		   "orr r1, r1, #0x13\n\t"
+		   "msr cpsr,r1 \n\t"
+		   
+	    :		/* output */
+	    : "r"(regs)	/* input */
+	    : "%r1"	/* clobbered register */
+	    );
+
+	return;
+}
+
+EXPORT_SYMBOL(kernel_sec_get_core_reg_dump);
+
+int kernel_sec_get_mmu_reg_dump(t_kernel_sec_mmu_info * mmu_info)
+{
+// CR DAC TTBR0 TTBR1 TTBCR
+	asm("mrc    p15, 0, r1, c1, c0, 0 \n\t"	//SCTLR
+	    "str r1, [%0] \n\t"
+	    "mrc    p15, 0, r1, c2, c0, 0 \n\t"	//TTBR0
+	    "str r1, [%0,#4] \n\t"
+	    "mrc    p15, 0, r1, c2, c0,1 \n\t"	//TTBR1
+	    "str r1, [%0,#8] \n\t"
+	    "mrc    p15, 0, r1, c2, c0,2 \n\t"	//TTBCR
+	    "str r1, [%0,#12] \n\t"
+	    "mrc    p15, 0, r1, c3, c0,0 \n\t"	//DACR
+	    "str r1, [%0,#16] \n\t"
+
+	    "mrc    p15, 0, r1, c5, c0,0 \n\t"	//DFSR
+	    "str r1, [%0,#20] \n\t"
+	    "mrc    p15, 0, r1, c6, c0,0 \n\t"	//DFAR
+	    "str r1, [%0,#24] \n\t"
+	    "mrc    p15, 0, r1, c5, c0,1 \n\t"	//IFSR
+	    "str r1, [%0,#28] \n\t"
+	    "mrc    p15, 0, r1, c6, c0,2 \n\t"	//IFAR
+	    "str r1, [%0,#32] \n\t"
+	    /*Dont populate DAFSR and RAFSR */
+	    "mrc    p15, 0, r1, c10, c2,0 \n\t"	//PMRRR
+	    "str r1, [%0,#44] \n\t"
+	    "mrc    p15, 0, r1, c10, c2,1 \n\t"	//NMRRR
+	    "str r1, [%0,#48] \n\t"
+	    "mrc    p15, 0, r1, c13, c0,0 \n\t"	//FCSEPID
+	    "str r1, [%0,#52] \n\t"
+	    "mrc    p15, 0, r1, c13, c0,1 \n\t"	//CONTEXT
+	    "str r1, [%0,#56] \n\t"
+	    "mrc    p15, 0, r1, c13, c0,2 \n\t"	//URWTPID
+	    "str r1, [%0,#60] \n\t"
+	    "mrc    p15, 0, r1, c13, c0,3 \n\t"	//UROTPID
+	    "str r1, [%0,#64] \n\t"
+	    "mrc    p15, 0, r1, c13, c0,4 \n\t"	//POTPIDR
+	    "str r1, [%0,#68] \n\t"
+
+	    :			/* output */
+	    : "r"(mmu_info)	/* input */
+	    : "%r1", "memory"	/* clobbered register */
+	    );
+	return 0;
+}
+
+EXPORT_SYMBOL(kernel_sec_get_mmu_reg_dump);
+
+void kernel_sec_save_final_context(void)
+{
+/*
+	if (kernel_sec_get_mmu_reg_dump(&kernel_sec_mmu_reg_dump) < 0) {
+		printk(KERN_EMERG
+		       "(kernel_sec_save_final_context) kernel_sec_get_mmu_reg_dump faile.\n");
+	}
+	kernel_sec_get_core_reg_dump(&kernel_sec_core_reg_dump);
+	printk(KERN_EMERG
+	       "(kernel_sec_save_final_context) Final context was saved before the system reset.\n");
+*/
+//	unsigned long flags;
+	
+//	local_irq_save(flags);
+	
+	kernel_sec_get_mmu_reg_dump(&per_cpu
+				(kernel_sec_mmu_reg_dump, smp_processor_id()));	
+	kernel_sec_get_core_reg_dump(&per_cpu
+				(kernel_sec_core_reg_dump, smp_processor_id()));
+	printk(KERN_EMERG
+	       "(%s) context saved(CPU:%d)\n", __func__, smp_processor_id());
+	       
+//	local_irq_restore(flags);
+}
+
+EXPORT_SYMBOL(kernel_sec_save_final_context);
+
+#endif	/* CONFIG_SAMSUNG_KERNEL_DEBUG */
 
 /**
  *	panic - halt the system
@@ -84,6 +384,8 @@ NORET_TYPE void panic(const char * fmt, ...)
 	dump_stack();
 #endif
 
+	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
+
 	/*
 	 * If we have crashed and we have a crash kernel loaded let it handle
 	 * everything else.
@@ -100,7 +402,42 @@ NORET_TYPE void panic(const char * fmt, ...)
 	 */
 	smp_send_stop();
 
-	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
+#ifdef CONFIG_SAMSUNG_KERNEL_DEBUG
+	
+		/*kernel_sec_save_final_context();*/
+
+	/* L1 & L2 cache management */
+	flush_cache_all();
+
+	if (cpu_is_u8500())
+		ux500_clean_l2_cache_all();
+
+	if (0 == strcmp(fmt,"__do_user_fault"))
+	{
+		printk(KERN_INFO "machine_restart: userlockup\n" ) ;
+		machine_restart("UserLockUp");
+	}
+	else if (0 == strcmp(fmt, "__forced_upload"))
+	{
+		printk(KERN_INFO "machine_restart: __forced_upload\n" ) ;
+		machine_restart("ForcedUpload");
+	}
+	else if (0 == strcmp(fmt, "__MMDSP_forced_upload"))
+	{
+		printk(KERN_INFO "machine_restart: __MMDSP_forced_upload\n" ) ;
+		machine_restart("MMDSP_ForcedUpload");
+	}
+	else
+	{
+		printk(KERN_INFO "machine_restart: lockup\n" ) ;
+		machine_restart("LockUp");
+	}
+
+	for (i = 0; i < 1*1000; i++) {
+		touch_nmi_watchdog();
+		mdelay(1);
+	}
+#endif /* CONFIG_SAMSUNG_KERNEL_DEBUG */
 
 	bust_spinlocks(0);
 
