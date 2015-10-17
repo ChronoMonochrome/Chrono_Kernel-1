dda6b5b1efbff1995326ebb0c4d3711e7d8c080a
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-18 03:25:51 +0400

    kernel: God Mode: initial introduction

---
+++ include/linux/god_mode.h
@@ -0,0 +1,21 @@
+/*
+ * Author: Shilin Victor aka ChronoMonochrome <chrono.monochrome@gmail.com>
+ *
+ * Copyright 2015 Shilin Victor
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _GOD_MODE_H
+#define _GOD_MODE_H
+
+extern bool god_mode_enabled;
+#endif
