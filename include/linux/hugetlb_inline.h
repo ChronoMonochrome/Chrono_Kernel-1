e8fbfcb3cb4d98f7bb3d609bcf990c329fed27b0
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-27 19:49:36 +0200

    make is_vm_hugetlb_page function __cold

--- include/linux/hugetlb_inline.h
+++ include/linux/hugetlb_inline.h
@@ -12,7 +12,7 @@ inline int is_vm_hugetlb_page(struct vm_area_struct *vma)
 
 #else
 
-inline int is_vm_hugetlb_page(struct vm_area_struct *vma)
+inline  __cold int is_vm_hugetlb_page(struct vm_area_struct *vma)
 {
 	return 0;
 }
