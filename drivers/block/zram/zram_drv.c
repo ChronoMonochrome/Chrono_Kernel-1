f84694fcdd239dba1e413ab6414479b4a0450c8b
Shilin Victor <chrono.monochrome@gmail.com>
2015-12-04 06:55:55 +0000

    zram: optimize for better compression speed
    - decrease max_zpage_size to PAGE_SIZE / 3;
    - use a small chunk of a page(PAGE_SIZE / 8) to compress instead of a whole one, to determine
    whether page should be stored in zram block uncompressed. Compressed size of this chunk is used
    to approximate, what size would have a whole page after compression.
    - add debug parameters discarded_total, discarded_useful, compressed, debug.
    
    Parameter discarded_total is amount of pages, approximated compressed size of which is higher than max_zpage_size.
    discarded_useful is number of those discarded pages, the real compressed size of which is higher than max_zpage_size.
    Note that this parameter is only working with debug set to 1 or higher value else no benefits here as it requires an
    original page to be compressed.
    
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -32,7 +32,7 @@
 #include <linux/sysfs.h>
 
 
-size_t max_zpage_size = PAGE_SIZE / 10 * 9;
+size_t max_zpage_size = PAGE_SIZE/3;
 module_param(max_zpage_size, uint, 0644);
 
 #include "zram_drv.h"
@@ -652,12 +652,21 @@ out_cleanup:
 	return ret;
 }
 
+static unsigned char page_chunk[PAGE_SIZE];
+static unsigned int debug = 0, discarded_total = 0, compressed = 0, discarded_useful = 0;
+
+module_param(debug, uint, 0644);
+module_param(discarded_total, uint, 0644);
+module_param(compressed, uint, 0644);
+module_param(discarded_useful, uint, 0644);
+
 static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 			   int offset)
 {
 	int ret = 0;
 	unsigned int clen;
 	unsigned long handle = 0;
+	size_t chunk_clen;
 	struct page *page;
 	unsigned char *user_mem, *cmem, *src, *uncmem = NULL;
 	struct zram_meta *meta = zram->meta;
@@ -705,8 +714,28 @@ compress_again:
 		goto out;
 	}
 
+	memset(page_chunk, 0, PAGE_SIZE);
+	memcpy(page_chunk, uncmem, PAGE_SIZE / 8);
+
 	zstrm = zcomp_stream_get(zram->comp);
-	ret = zcomp_compress(zstrm, uncmem, &clen);
+	ret = zcomp_compress(zstrm, page_chunk, &chunk_clen);
+
+	if (chunk_clen * 8 <= max_zpage_size || debug) {
+		ret = zcomp_compress(zstrm, uncmem, &clen);
+	} else {
+		clen = PAGE_SIZE;
+	}
+
+	if (chunk_clen * 8 > max_zpage_size) {
+		discarded_total++;
+		if (clen > max_zpage_size && debug)
+			discarded_useful++;
+	} else
+		compressed++;
+
+	if (debug > 1)
+		pr_err("[zram] chunk_clen=%d, approx=%d, real=%d\n", chunk_clen, chunk_clen * 8, clen);
+
 	if (!is_partial_io(bvec)) {
 		kunmap_atomic(user_mem);
 		user_mem = NULL;
