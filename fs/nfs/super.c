34d78b545cf5bac794efb265e5998363426d4f14
Shilin Victor <chrono.monochrome@gmail.com>
2015-12-11 05:31:01 +0000

    nfs: fix build

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -771,7 +771,7 @@ static int nfs_show_devname(struct seq_file *m, struct dentry *root)
 	int err = 0;
 	if (!page)
 		return -ENOMEM;
-	devname = nfs_path(&dummy, root, page, PAGE_SIZE, 0);
+	devname = nfs_path(&dummy, root, page, PAGE_SIZE);
 	if (IS_ERR(devname))
 		err = PTR_ERR(devname);
 	else
