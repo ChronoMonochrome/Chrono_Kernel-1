6c2d98b0f77e8089690c78e386fb955c86cf9d11
Ulquiorra Schiffer <UlquiorraSchiffer00000@gmail.com>
2016-01-04 06:56:54 +0000

    plist, futex: reset to android-3.4

--- kernel/fork.c
+++ kernel/fork.c
@@ -1147,7 +1147,7 @@ static void rt_mutex_init_task(struct task_struct *p)
 {
 	raw_spin_lock_init(&p->pi_lock);
 #ifdef CONFIG_RT_MUTEXES
-	plist_head_init(&p->pi_waiters, NULL);
+	plist_head_init(&p->pi_waiters);
 	p->pi_blocked_on = NULL;
 #endif
 }
