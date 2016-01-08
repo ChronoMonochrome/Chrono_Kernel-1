7404372701e4551a803cbdf36527f44b4077ca4f
Shilin Victor <Chrono.Monochrome@gmail.com>
2016-01-08 15:43:39 +0300

    alarm: apply device-specific patches

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -68,6 +68,10 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	enum android_alarm_type alarm_type = ANDROID_ALARM_IOCTL_TO_TYPE(cmd);
 	uint32_t alarm_type_mask = 1U << alarm_type;
 
+#if defined(CONFIG_MACH_SEC_GOLDEN_CHN) || defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined (CONFIG_MACH_GAVINI_CHN)
+	char bootalarm_data[14];
+#endif
+
 	if (alarm_type >= ANDROID_ALARM_TYPE_COUNT)
 		return -EINVAL;
 
@@ -161,6 +165,18 @@ from_old_alarm_set:
 		if (rv < 0)
 			goto err1;
 		break;
+
+#if defined(CONFIG_MACH_SEC_GOLDEN_CHN) || defined(CONFIG_MACH_JANICE_CHN) || defined(CONFIG_MACH_CODINA_CHN) || defined(CONFIG_MACH_GAVINI_CHN)
+	case ANDROID_ALARM_SET_ALARM:
+		if (copy_from_user(bootalarm_data,
+				(void __user *)arg, 14)) {
+			pr_alarm(IO, "Power off alarm error!\n");
+			rv = -EFAULT;
+			goto err1;
+		}
+		rv = alarm_set_alarm(bootalarm_data);
+	break;
+#endif
 	case ANDROID_ALARM_GET_TIME(0):
 		switch (alarm_type) {
 		case ANDROID_ALARM_RTC_WAKEUP:
