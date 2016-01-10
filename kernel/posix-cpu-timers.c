e8a8eb3d6140f7f2492fc45b5494c2c7c9038b52
Shilin Victor <chrono@monochrome.com>
2016-01-10 15:05:42 +0100

    posix-cpu-timers: use raw_spin_lock_irqsave

--- kernel/posix-cpu-timers.c
+++ kernel/posix-cpu-timers.c
@@ -283,11 +283,11 @@ void thread_group_cputimer(struct task_struct *tsk, struct task_cputime *times)
 		 * it.
 		 */
 		thread_group_cputime(tsk, &sum);
-		spin_lock_irqsave(&cputimer->lock, flags);
+		raw_spin_lock_irqsave(&cputimer->lock, flags);
 		cputimer->running = 1;
 		update_gt_cputime(&cputimer->cputime, &sum);
 	} else
-		spin_lock_irqsave(&cputimer->lock, flags);
+		raw_spin_lock_irqsave(&cputimer->lock, flags);
 	*times = cputimer->cputime;
 	raw_spin_unlock_irqrestore(&cputimer->lock, flags);
 }
