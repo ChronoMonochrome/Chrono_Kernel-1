5c63fd26b2ca1074cc03a30d4226fdd2e1714b4b
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-16 03:32:41 +0100

    exfat: fix compilation

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -1640,14 +1640,8 @@ static int exfat_write_end(struct file *file, struct address_space *mapping,
 }
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,16,0)
-#ifdef CONFIG_AIO_OPTIMIZATION
 static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 						struct iov_iter *iter, loff_t offset)
-#else
-static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
-					   const struct iovec *iov,
-					   loff_t offset, unsigned long nr_segs)
-#endif
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(4,2,0)
 static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 					   struct iov_iter *iter, loff_t offset)
@@ -1669,12 +1663,8 @@ static ssize_t exfat_direct_IO(struct kiocb *iocb,
 
 	if (rw == WRITE) {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,16,0)
-#ifdef CONFIG_AIO_OPTIMIZATION
 		if (EXFAT_I(inode)->mmu_private <
 					(offset + iov_iter_count(iter)))
-#else
-		if (EXFAT_I(inode)->mmu_private < (offset + iov_length(iov, nr_segs)))
-#endif
 #else
 		if (EXFAT_I(inode)->mmu_private < (offset + iov_iter_count(iter)))
 #endif
@@ -1687,13 +1677,8 @@ static ssize_t exfat_direct_IO(struct kiocb *iocb,
 	ret = blockdev_direct_IO(rw, iocb, inode, iter,
 					offset, exfat_get_block);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)
-#ifdef CONFIG_AIO_OPTIMIZATION
 	ret = blockdev_direct_IO(rw, iocb, inode, iter,
 					offset, exfat_get_block);
-#else
-	ret = blockdev_direct_IO(rw, iocb, inode, iov,
-					offset, nr_segs, exfat_get_block);
-#endif
 #else
         ret = blockdev_direct_IO(rw, iocb, inode, inode->i_sb->s_bdev, iov,
 					offset, nr_segs, exfat_get_block, NULL);
@@ -1704,11 +1689,7 @@ static ssize_t exfat_direct_IO(struct kiocb *iocb,
 		exfat_write_failed(mapping, offset+iov_iter_count(iter));
 #elif LINUX_VERSION_CODE > KERNEL_VERSION(2,6,34)
 	if ((ret < 0) && (rw & WRITE))
-#ifdef CONFIG_AIO_OPTIMIZATION
 		exfat_write_failed(mapping, offset+iov_iter_count(iter));
-#else
-		exfat_write_failed(mapping, offset+iov_length(iov, nr_segs));
-#endif
 #endif
 	return ret;
 }
