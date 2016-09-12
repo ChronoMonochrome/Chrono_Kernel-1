052660a78db752bdcf4d006f8a6cc3c644acab20
Shilin Victor <chrono.monochrome@gmail.com>
2016-09-12 20:34:58 +0700

    security: revert changes

---
+++ security/selinux/include/conditional.h
@@ -0,0 +1,25 @@
+#ifdef CONFIG_GOD_MODE
+#include <linux/god_mode.h>
+#endif
+/*
+ * Interface to booleans in the security server. This is exported
+ * for the selinuxfs.
+ *
+ * Author: Karl MacMillan <kmacmillan@tresys.com>
+ *
+ * Copyright (C) 2003 - 2004 Tresys Technology, LLC
+ *	This program is free software; you can redistribute it and/or modify
+ *  	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, version 2.
+ */
+
+#ifndef _SELINUX_CONDITIONAL_H_
+#define _SELINUX_CONDITIONAL_H_
+
+int security_get_bools(int *len, char ***names, int **values);
+
+int security_set_bools(int len, int *values);
+
+int security_get_bool_value(int bool);
+
+#endif
