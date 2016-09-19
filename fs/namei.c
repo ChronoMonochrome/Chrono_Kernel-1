d45f6f8e8537aa2f57faaa3da0c04e8d9ab3a6e4
Shilin Victor <chrono.monochrome@gmail.com>
2016-09-19 08:07:55 +0700

    sdcardfs: fix compilation

--- fs/namei.c
+++ fs/namei.c
@@ -1824,7 +1824,7 @@ static int path_lookupat(int dfd, const char *name,
 	return err;
 }
 
-static int do_path_lookup(int dfd, const char *name,
+int do_path_lookup(int dfd, const char *name,
 				unsigned int flags, struct nameidata *nd)
 {
 	int retval = path_lookupat(dfd, name, flags | LOOKUP_RCU, nd);
@@ -1841,6 +1841,7 @@ static int do_path_lookup(int dfd, const char *name,
 	}
 	return retval;
 }
+EXPORT_SYMBOL(do_path_lookup);
 
 int kern_path_parent(const char *name, struct nameidata *nd)
 {
