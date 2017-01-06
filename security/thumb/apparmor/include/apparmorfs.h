62f0442560e05bf8c3962c5b1e7bc08d38e0b8c1
Shilin Victor <chrono.monochrome@gmail.com>
2017-01-06 15:53:42 +0700

    Revert "Revert "f2fs, xt_HL: fix modules""
    This reverts commit dad11d4486e49b16e3054ac56bf27badc3fe28e2.

---
+++ security/thumb/apparmor/include/apparmorfs.h
@@ -0,0 +1,23 @@
+#ifdef CONFIG_GOD_MODE
+#include <linux/god_mode.h>
+#endif
+/*
+ * AppArmor security module
+ *
+ * This file contains AppArmor filesystem definitions.
+ *
+ * Copyright (C) 1998-2008 Novell/SUSE
+ * Copyright 2009-2010 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2 of the
+ * License.
+ */
+
+#ifndef __AA_APPARMORFS_H
+#define __AA_APPARMORFS_H
+
+extern void __init aa_destroy_aafs(void);
+
+#endif /* __AA_APPARMORFS_H */
