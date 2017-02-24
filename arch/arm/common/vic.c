8a1ebc4a9a79c8a6405148de6ce9720c4d7e4a88
Shilin Victor <chrono.monochrome@gmail.com>
2017-02-24 13:15:38 +0700

    Fixes

--- arch/arm/common/vic.c
+++ arch/arm/common/vic.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <linux/export.h>
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/list.h>
 #include <linux/io.h>
