caa8964a3414d6d53a8878477b4fd35ef25659d4
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-25 18:01:28 +0100

    drivers/clk

--- include/linux/clkdev.h
+++ include/linux/clkdev.h
@@ -24,13 +24,6 @@ struct clk_lookup {
 	struct clk		*clk;
 };
 
-#define CLKDEV_INIT(d, n, c)	\
-	{			\
-		.dev_id = d,	\
-		.con_id = n,	\
-		.clk = c,	\
-	}
-
 struct clk_lookup *clkdev_alloc(struct clk *clk, const char *con_id,
 	const char *dev_fmt, ...);
 
