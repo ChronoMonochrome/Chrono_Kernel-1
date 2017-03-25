e45e75a04a43876efce5d0589f9b34f16b66b19f
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-25 17:49:03 +0100

    drivers/leds/

--- drivers/leds/leds-cobalt-raq.c
+++ drivers/leds/leds-cobalt-raq.c
@@ -24,7 +24,6 @@
 #include <linux/platform_device.h>
 #include <linux/spinlock.h>
 #include <linux/types.h>
-#include <linux/export.h>
 
 #define LED_WEB		0x04
 #define LED_POWER_OFF	0x08
--- drivers/leds/leds-fsg.c
+++ drivers/leds/leds-fsg.c
@@ -19,7 +19,6 @@
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/leds.h>
-#include <linux/module.h>
 #include <mach/hardware.h>
 #include <asm/io.h>
 
@@ -224,7 +223,20 @@ static struct platform_driver fsg_led_driver = {
 	},
 };
 
-module_platform_driver(fsg_led_driver);
+
+static int __init fsg_led_init(void)
+{
+	return platform_driver_register(&fsg_led_driver);
+}
+
+static void __exit fsg_led_exit(void)
+{
+	platform_driver_unregister(&fsg_led_driver);
+}
+
+
+module_init(fsg_led_init);
+module_exit(fsg_led_exit);
 
 MODULE_AUTHOR("Rod Whitby <rod@whitby.id.au>");
 MODULE_DESCRIPTION("Freecom FSG-3 LED driver");
