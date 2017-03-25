dc63bb5d9dfacc2aa6ea2859940d57a9512d47f1
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-25 17:50:21 +0100

    drivers/platform/
    Conflicts:
    	drivers/platform/x86/asus-wmi.c
    	drivers/platform/x86/samsung-laptop.c

--- drivers/platform/x86/intel_scu_ipcutil.c
+++ drivers/platform/x86/intel_scu_ipcutil.c
@@ -24,12 +24,15 @@
 #include <linux/init.h>
 #include <asm/intel_scu_ipc.h>
 
-static int major;
+static u32 major;
+
+#define MAX_FW_SIZE 264192
 
 /* ioctl commnds */
 #define	INTE_SCU_IPC_REGISTER_READ	0
 #define INTE_SCU_IPC_REGISTER_WRITE	1
 #define INTE_SCU_IPC_REGISTER_UPDATE	2
+#define INTE_SCU_IPC_FW_UPDATE		0xA2
 
 struct scu_ipc_data {
 	u32     count;  /* No. of registers */
@@ -85,14 +88,27 @@ static long scu_ipc_ioctl(struct file *fp, unsigned int cmd,
 	if (!capable(CAP_SYS_RAWIO))
 		return -EPERM;
 
-	if (copy_from_user(&data, argp, sizeof(struct scu_ipc_data)))
-		return -EFAULT;
-	ret = scu_reg_access(cmd, &data);
-	if (ret < 0)
-		return ret;
-	if (copy_to_user(argp, &data, sizeof(struct scu_ipc_data)))
-		return -EFAULT;
-	return 0;
+	if (cmd == INTE_SCU_IPC_FW_UPDATE) {
+			u8 *fwbuf = kmalloc(MAX_FW_SIZE, GFP_KERNEL);
+			if (fwbuf == NULL)
+				return -ENOMEM;
+			if (copy_from_user(fwbuf, (u8 *)arg, MAX_FW_SIZE)) {
+				kfree(fwbuf);
+				return -EFAULT;
+			}
+			ret = intel_scu_ipc_fw_update(fwbuf, MAX_FW_SIZE);
+			kfree(fwbuf);
+			return ret;
+	} else {
+		if (copy_from_user(&data, argp, sizeof(struct scu_ipc_data)))
+			return -EFAULT;
+		ret = scu_reg_access(cmd, &data);
+		if (ret < 0)
+			return ret;
+		if (copy_to_user(argp, &data, sizeof(struct scu_ipc_data)))
+			return -EFAULT;
+		return 0;
+	}
 }
 
 static const struct file_operations scu_ipc_fops = {
@@ -101,11 +117,7 @@ static const struct file_operations scu_ipc_fops = {
 
 static int __init ipc_module_init(void)
 {
-	major = register_chrdev(0, "intel_mid_scu", &scu_ipc_fops);
-	if (major < 0)
-		return major;
-
-	return 0;
+	return register_chrdev(0, "intel_mid_scu", &scu_ipc_fops);
 }
 
 static void __exit ipc_module_exit(void)
