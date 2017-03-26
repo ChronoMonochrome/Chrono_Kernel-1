073e9c0646c5a68d65a0d9e6922c5611c43e9677
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 14:25:29 +0200

    codina: move kexec page to the end of RAM
    Conflicts:
    	arch/arm/mach-ux500/include/mach/memory.h

--- arch/arm/mach-ux500/include/mach/memory.h
+++ arch/arm/mach-ux500/include/mach/memory.h
@@ -15,13 +15,13 @@
 #define PLAT_PHYS_OFFSET	UL(0x00000000)
 #define BUS_OFFSET	UL(0x00000000)
 
-#if defined(CONFIG_MACH_U8500_LOTUS) || defined(CONFIG_MACH_U8500_PEPPER) || defined(CONFIG_MACH_CODINA)
-#define END_MEM		            UL(0x1FDFFFFF)
+#if defined(CONFIG_MACH_CODINA)
+#define END_MEM		            UL(0x2FEFFFFF)
 #endif
 
 #if defined(CONFIG_KEXEC_HARDBOOT)
-#if defined(CONFIG_MACH_U8500_LOTUS) || defined(CONFIG_MACH_U8500_PEPPER) || defined(CONFIG_MACH_CODINA)
-#define KEXEC_HB_PAGE_ADDR		UL(0x1FE00000)
+#if defined(CONFIG_MACH_CODINA)
+#define KEXEC_HB_PAGE_ADDR		UL(0x2fff6000)
 #else
 #error "Address for kexec hardboot page not defined"
 #endif
