c665f57e4bb76aaa81a2150e835df6bc2c73bba3
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 14:12:53 +0200

    irq, random: match with LK3.4 API
    Conflicts:
    	drivers/char/random.c
    	kernel/irq/manage.c
    
    Conflicts:
    	kernel/irq/manage.c

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -804,8 +804,6 @@ static int irq_thread(void *data)
 	sched_setscheduler(current, SCHED_FIFO, &param);
 	current->irq_thread = 1;
 
-	irq_thread_check_affinity(desc, action);
-
 	while (!irq_wait_for_interrupt(action)) {
 		irqreturn_t action_ret;
 
@@ -1035,7 +1033,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		 * thread_mask assigned. See the loop above which or's
 		 * all existing action->thread_mask bits.
 		 */
-		new->thread_mask = 1UL << ffz(thread_mask);
+		new->thread_mask = 1 << ffz(thread_mask);
 	}
 
 	if (!shared) {
