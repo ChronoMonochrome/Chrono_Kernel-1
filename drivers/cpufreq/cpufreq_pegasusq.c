116cba0bab7441b51aacaf8a1e025c000aabbbcf
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 09:23:44 +0200

    Tweak PegasusQ tunables
    Based on values provided by ace2nutzer@xda-developers.com modifications

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -150,7 +150,7 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_SAMPLING_DOWN_FACTOR		(2)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(5)
-#define DEF_FREQUENCY_UP_THRESHOLD		(75)
+#define DEF_FREQUENCY_UP_THRESHOLD		(99)
 
 /* for multiple freq_step */
 #define DEF_UP_THRESHOLD_DIFF			(5)
@@ -158,7 +158,7 @@ static unsigned int get_nr_run_avg(void)
 #define DEF_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
-#define DEF_SAMPLING_RATE			(50000)
+#define DEF_SAMPLING_RATE			(30000)
 #define MIN_SAMPLING_RATE			(10000)
 #define MAX_HOTPLUG_RATE			(40u)
 
@@ -175,8 +175,8 @@ static unsigned int get_nr_run_avg(void)
 
 #define DEF_START_DELAY				(0)
 
-#define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(400000)
+#define UP_THRESHOLD_AT_MIN_FREQ		(80)
+#define FREQ_FOR_RESPONSIVENESS			(600000)
 /* for fast decrease */
 #define FREQ_FOR_FAST_DOWN			(800000)
 #define UP_THRESHOLD_AT_FAST_DOWN		(95)
