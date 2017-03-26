f6f49e388a4291122b3c20bf21de45dc5de1b643
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 14:16:38 +0200

    MCDE: reset to 24a7603
    Conflicts:
    	drivers/video/mcde/mcde_hw.c

--- drivers/video/mcde/display-s6e63m0.c
+++ drivers/video/mcde/display-s6e63m0.c
@@ -1260,11 +1260,11 @@ unsigned short *Gen_gamma_table(struct s6e63m0 *lcd)
 
 	/* for debug */
 	#if 0
-		printk("%s lcd->bl : %d ",__func__,lcd->bl);
+;
 		for(i=3;i<((gen_table_max*2)+3);i+=2) {
-			printk("0x%x ",s6e63m0_22_gamma_table[i]);
+;
 		}
-		printk("\n");
+;
 	#endif
 
 	if (R_req) {
@@ -1660,8 +1660,8 @@ static void s6e63mo_read_mtp_info(struct s6e63m0 *lcd)
 				(u8 *)(&(lcd->smart.MTP)), LDI_MTP_LENGTH);
 
 	for(i=0;i<LDI_MTP_LENGTH;i++) {
-		printk("[S6E63M0] MainMTPData [%d] : %02x\n", i,
-				((char*)&(lcd->smart.MTP))[i]);
+//		printk("[S6E63M0] MainMTPData [%d] : %02x\n", i,
+;
 	}
 
 	Smart_dimming_init(&(lcd->smart));
@@ -1675,8 +1675,8 @@ static void s6e63mo_mtp_from_boot(struct s6e63m0 *lcd, char *mtp)
 	memcpy(&(lcd->smart.MTP), mtp, LDI_MTP_LENGTH);
 
 	for (i = 0; i < LDI_MTP_LENGTH; i++) {
-		printk("[S6E63M0] MainMTPData [%d] %02x\n", i,
-				((char *)&(lcd->smart.MTP))[i]);
+//		printk("[S6E63M0] MainMTPData [%d] %02x\n", i,
+;
 	}
 	Smart_dimming_init(&(lcd->smart));
 }
@@ -1702,7 +1702,7 @@ static int update_brightness(struct s6e63m0 *lcd, u8 force)
 	if ((force) || ((lcd->ldi_state) &&
 				(lcd->current_brightness != lcd->bl))) {
 
-	printk("[S6E63M0] Brightness: %d BL: %d\n", bl, lcd->bl);
+;
 
 		ret = s6e63m0_set_elvss(lcd);
 		if (ret) {
@@ -2090,7 +2090,7 @@ static ssize_t s6e63m0_sysfs_store_gamma_mode(struct device *dev,
 	if (lcd->ldi_state)
 	{
 		if((lcd->current_brightness == lcd->bl) && (lcd->current_gamma_mode == lcd->gamma_mode))
-			printk("there is no gamma_mode & brightness changed\n");
+;
 		else
 			s6e63m0_gamma_ctl(lcd);
 	}
--- drivers/video/mcde/mcde_hw.c
+++ drivers/video/mcde/mcde_hw.c
@@ -1174,6 +1174,7 @@ static int wait_for_vsync(struct mcde_chnl_state *chnl)
 	}
 }
 
+#ifdef CONFIG_MCDE_LCDCLK_MANAGEMENT
 /* PRCMU LCDCLK */
 /* 60+++	79872000 unsafe
  * 60++ 	62400000 unsafe
@@ -1187,6 +1188,8 @@ static int wait_for_vsync(struct mcde_chnl_state *chnl)
 #include <linux/mfd/dbx500-prcmu.h>
 #include <linux/mfd/db8500-prcmu.h>
 
+extern int lcdclk_usr;
+
 #define LCDCLK_SET(clk) prcmu_set_clock_rate(PRCMU_LCDCLK, (unsigned long) clk);
 
 struct lcdclk_prop
@@ -1197,32 +1200,31 @@ struct lcdclk_prop
 
 static struct lcdclk_prop lcdclk_prop[] = {
   	[0] = {
-		.name = "60++ Hz (unsafe)",
-		.clk = 66560000,
-	},
-  	[1] = {
 		.name = "60+ Hz (unsafe)",
 		.clk = 57051428,
 	},
-	[2] = {
+	[1] = {
 		.name = "60 Hz",
 		.clk = 49920000,
 	},
-	[3] = {
+	[2] = {
 		.name = "50 Hz",
 		.clk = 39936000,
 	},
-	[4] = {
+	[3] = {
 		.name = "45 Hz",
 		.clk = 36305454,
 	},
-	[5] = {
+	[4] = {
 		.name = "40 Hz",
 		.clk = 33280000,
 	},
+  	[5] = {
+		.name = "35 Hz",
+		.clk = 30720000,
+	},
 };
 
-static int lcdclk_usr = 2; /* 60 Hz */
 static unsigned int custom_lcdclk = 49920000;
 
 static void lcdclk_thread(struct work_struct *ws2401_lcdclk_work)
@@ -1232,7 +1234,7 @@ static void lcdclk_thread(struct work_struct *ws2401_lcdclk_work)
 	if ((custom_lcdclk != 0) && (lcdclk_usr == -2)) {
 		pr_err("[MCDE] LCDCLK %dHz\n", custom_lcdclk);
 		LCDCLK_SET(custom_lcdclk);
-	} else if (lcdclk_usr != -1) { 
+	} else if (lcdclk_usr != -1) {
 		pr_err("[MCDE] LCDCLK %dHz\n", lcdclk_prop[lcdclk_usr].clk);
 		LCDCLK_SET(lcdclk_prop[lcdclk_usr].clk);
 	}
@@ -1248,23 +1250,30 @@ static DECLARE_WORK(lcdclk_work, lcdclk_thread);
 #define ATTR_RW(_name)	\
 	static struct kobj_attribute _name##_interface = __ATTR(_name, 0644, _name##_show, _name##_store);
 
-static ssize_t mcde_lcdclk_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+extern bool is_s6d(void);
+
+static ssize_t lcdclk_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	int i;
 
+        if (is_s6d())
+                sprintf(buf, "%sLCD type: %s\n", buf,  "S6D27A1");
+        else
+                sprintf(buf, "%sLCD type: %s\n", buf,  "WS2401");
+
 	sprintf(buf, "%s[-2][%s] Custom\n", buf, lcdclk_usr == -2 ? "*" : " ");
 	sprintf(buf, "%s[-1][%s] Default (60 Hz)\n", buf, lcdclk_usr == -1 ? "*" : " ");
-	
+
 	for (i = 0; i < ARRAY_SIZE(lcdclk_prop); i++) {
 		sprintf(buf, "%s[%d][%s] %s\n", buf, i, i == lcdclk_usr ? "*" : " ", lcdclk_prop[i].name);
 	}
-	
+
 	sprintf(buf, "%sCurrent LCDCLK freq: %d\n", buf, (int) prcmu_clock_rate(PRCMU_LCDCLK));
 
 	return strlen(buf);
 }
 
-static ssize_t mcde_lcdclk_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+static ssize_t lcdclk_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 	int ret, tmp;
 	
@@ -1275,7 +1284,7 @@ static ssize_t mcde_lcdclk_store(struct kobject *kobj, struct kobj_attribute *at
 	}
 
 	ret = sscanf(buf, "%d", &tmp);
-	if (!ret || (tmp < -2) || (tmp > 5)) {
+	if (!ret || (tmp < -2) || (tmp > ARRAY_SIZE(lcdclk_prop) - 1)) {
 		  pr_err("[MCDE] Bad cmd\n");
 		  return -EINVAL;
 	}
@@ -1286,11 +1295,11 @@ out:
 
 	return count;
 }
-ATTR_RW(mcde_lcdclk);
+ATTR_RW(lcdclk);
 
 static struct attribute *mcde_attrs[] = {
  
-	&mcde_lcdclk_interface.attr, 
+	&lcdclk_interface.attr, 
 	NULL,
 };
 
@@ -1300,6 +1309,7 @@ static struct attribute_group mcde_interface_group = {
 };
 
 static struct kobject *mcde_kobject;
+#endif /* CONFIG_MCDE_LCDCLK_MANAGEMENT */
 
 static int update_channel_static_registers(struct mcde_chnl_state *chnl)
 {
@@ -1391,10 +1401,12 @@ static int update_channel_static_registers(struct mcde_chnl_state *chnl)
 	}
 
 	if (port->type == MCDE_PORTTYPE_DPI) {
+#ifdef CONFIG_MCDE_LCDCLK_MANAGEMENT
 		if (lcdclk_usr != -1) {
 			pr_err("[MCDE] Rebasing LCDCLK...\n");
 			schedule_work(&lcdclk_work);
 		}
+#endif
 		
 		if (port->phy.dpi.lcd_freq != clk_round_rate(chnl->clk_dpi,
 						port->phy.dpi.lcd_freq))
@@ -1403,8 +1415,9 @@ static int update_channel_static_registers(struct mcde_chnl_state *chnl)
 					
 		WARN_ON_ONCE(clk_set_rate(chnl->clk_dpi,
 					port->phy.dpi.lcd_freq));
+#ifdef CONFIG_MCDE_LCDCLK_MANAGEMENT
 		pr_err("[MCDE] rebased LCDCLK to %d Hz\n", port->phy.dpi.lcd_freq);
-		
+#endif		
 		WARN_ON_ONCE(clk_enable(chnl->clk_dpi));
 	}
 
@@ -3515,6 +3528,7 @@ int mcde_chnl_set_rotation(struct mcde_chnl_state *chnl,
 
 	return 0;
 }
+EXPORT_SYMBOL(mcde_chnl_set_rotation);
 
 int mcde_chnl_set_power_mode(struct mcde_chnl_state *chnl,
 				enum mcde_display_power_mode power_mode)
@@ -3552,6 +3566,7 @@ int mcde_chnl_apply(struct mcde_chnl_state *chnl)
 
 	return ret;
 }
+EXPORT_SYMBOL(mcde_chnl_apply);
 
 int mcde_chnl_update(struct mcde_chnl_state *chnl,
 					bool tripple_buffer)
@@ -3579,6 +3594,7 @@ int mcde_chnl_update(struct mcde_chnl_state *chnl,
 
 	return ret;
 }
+EXPORT_SYMBOL(mcde_chnl_update);
 
 void mcde_chnl_put(struct mcde_chnl_state *chnl)
 {
@@ -3604,6 +3620,7 @@ void mcde_chnl_put(struct mcde_chnl_state *chnl)
 
 	dev_vdbg(&mcde_dev->dev, "%s exit\n", __func__);
 }
+EXPORT_SYMBOL(mcde_chnl_put);
 
 void mcde_chnl_stop_flow(struct mcde_chnl_state *chnl)
 {
@@ -3616,6 +3633,7 @@ void mcde_chnl_stop_flow(struct mcde_chnl_state *chnl)
 
 	dev_vdbg(&mcde_dev->dev, "%s exit\n", __func__);
 }
+EXPORT_SYMBOL(mcde_chnl_stop_flow);
 
 void mcde_chnl_enable(struct mcde_chnl_state *chnl)
 {
@@ -3627,6 +3645,7 @@ void mcde_chnl_enable(struct mcde_chnl_state *chnl)
 
 	dev_vdbg(&mcde_dev->dev, "%s exit\n", __func__);
 }
+EXPORT_SYMBOL(mcde_chnl_enable);
 
 void mcde_chnl_disable(struct mcde_chnl_state *chnl)
 {
@@ -3642,6 +3661,7 @@ void mcde_chnl_disable(struct mcde_chnl_state *chnl)
 
 	dev_vdbg(&mcde_dev->dev, "%s exit\n", __func__);
 }
+EXPORT_SYMBOL(mcde_chnl_disable);
 
 void mcde_formatter_enable(struct mcde_chnl_state *chnl)
 {
@@ -4250,7 +4270,7 @@ static void mcde_underflow_function(struct work_struct *ptr)
 	ret = mcde_suspend(dev, dummy);
 	if (ret < 0) {
 		dev_err(dev, "mcde_suspend() failed ret=%d\n", ret);
-		printk(KERN_INFO "mcde_suspend() failed ret=%d\n", ret);
+;
 		goto suspend_failed;
 	}
 
@@ -4260,11 +4280,11 @@ static void mcde_underflow_function(struct work_struct *ptr)
 	ret = mcde_resume(dev);
 	if (ret == 0) {
 		dev_info(dev, "%s: mcde recovered\n", __func__);
-		printk(KERN_INFO "%s: mcde recovered\n", __func__);
+;
 	}
 	else {
 		dev_err(dev, "mcde_resume() failed ret=%d\n", ret);
-		printk(KERN_INFO "mcde_resume() failed ret=%d\n", ret);
+;
 	}
 
 	update_area.x = 0;
@@ -4274,12 +4294,12 @@ static void mcde_underflow_function(struct work_struct *ptr)
 	ret = mcde_chnl_update(chnl, &update_area, 0 /* tripple_buffer */);
 	if (ret < 0) {
 		dev_err(dev, "mcde_chnl_update() failed ret=%d\n", ret);
-		printk(KERN_INFO "mcde_chnl_update() failed ret=%d\n", ret);
+;
 	}
 
 suspend_failed:
 	dev_vdbg(dev, "%s: resume b2r2\n", __func__);
-	printk(KERN_INFO "%s: resume b2r2\n", __func__);
+;
 	/* args are not used, always returns 0 */
 	b2r2_resume(NULL);
 #endif
@@ -4462,6 +4482,7 @@ int __init mcde_init(void)
   
 	mutex_init(&mcde_hw_lock);
 	
+#ifdef CONFIG_MCDE_LCDCLK_MANAGEMENT
 	mcde_kobject = kobject_create_and_add("mcde", kernel_kobj);
 	if (!mcde_kobject) {
 		pr_err("[MCDE] Failed to create kobject interface\n");
@@ -4472,6 +4493,7 @@ int __init mcde_init(void)
 	if (ret) {
 		kobject_put(mcde_kobject);
 	}
+#endif
 	
 out:
 	return platform_driver_register(&mcde_driver);
@@ -4480,6 +4502,8 @@ out:
 void mcde_exit(void)
 {
 	/* REVIEW: shutdown MCDE? */
+#ifdef CONFIG_MCDE_LCDCLK_MANAGEMENT
 	kobject_put(mcde_kobject);
+#endif
 	platform_driver_unregister(&mcde_driver);
 }
