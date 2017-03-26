c17c09a071078408a3e05583b7418c1df18c53e0
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 07:54:05 +0200

    tspdrv: sysfs interface to tweak vibration intensity
    Strongest vibration = 0
    Weakest vibration = 60
    
    Change the value in "sys/vibrator/pwm_value" between 0 and 60.

--- drivers/misc/tspdrv/tspdrv.c
+++ drivers/misc/tspdrv/tspdrv.c
@@ -7,6 +7,7 @@
 **     TouchSense Kernel Module main entry-point.
 **
 ** Portions Copyright (c) 2008-2010 Immersion Corporation. All Rights Reserved.
+**          Copyright (c) 2014 Davide Pianca <kingbabasula@gmail.com>
 **
 ** This file contains Original Code and/or Modifications of Original Code
 ** as defined in and that are subject to the GNU Public License v2 -
@@ -73,6 +74,8 @@ static struct vibrator {
 	struct work_struct work;
 	bool running;
 } vibdata;
+
+unsigned long pwm_val = 10;
 
 /* Uncomment the next line to enable debug prints */
 /* #define	IMMVIBE_DEBUG */
@@ -251,7 +254,7 @@ static void janice_vibrator_enable(struct timed_output_dev *dev, int value)
 			/* PWM generation mode */
 			immvibe_i2c_write(isa_data->client, HCTRL0, 0x91);
 			/* Duty 0x64 == nForce 90 */
-			immvibe_i2c_write(isa_data->client, HCTRL5, 0x64);
+			immvibe_i2c_write(isa_data->client, HCTRL5, pwm_val);
 
 			vibdata.running = true;
 		} else
@@ -313,7 +316,7 @@ int init_tspdrv_module(void)
 	DbgRecorderInit(());
 
 	ImmVibeSPI_ForceOut_Initialize();
-    VibeOSKernelLinuxInitTimer();
+       VibeOSKernelLinuxInitTimer();
 
 	/* Get and concatenate device name and initialize data buffer */
 	g_cchDeviceName = 0;
@@ -711,6 +714,12 @@ static int __devinit immvibe_i2c_probe(struct i2c_client* client, const struct i
 	ret = init_tspdrv_module();
 	if (ret) {
 		viberr("%s():Error initializing tspdrv", __func__);
+		goto out_tspdrv_init_failed;
+	}
+
+	ret = create_sysfs();
+	if (ret) {
+		pr_err("Failed to create sysfs interface", ret);
 		goto out_tspdrv_init_failed;
 	}
 
