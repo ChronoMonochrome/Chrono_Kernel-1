e2eeb61ac0e185723b1d8cd896dc872fa2c18873
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 08:11:26 +0200

    libcrc32c: compile with -fstrict-aliasing

--- lib/libcrc32c.c
+++ lib/libcrc32c.c
@@ -43,18 +43,18 @@ u32 crc32c(u32 crc, const void *address, unsigned int length)
 {
 	struct {
 		struct shash_desc shash;
-		char ctx[crypto_shash_descsize(tfm)];
+		u32 ctx[crypto_shash_descsize(tfm)];
 	} desc;
 	int err;
 
 	desc.shash.tfm = tfm;
 	desc.shash.flags = 0;
-	*(u32 *)desc.ctx = crc;
+	*desc.ctx = crc;
 
 	err = crypto_shash_update(&desc.shash, address, length);
 	BUG_ON(err);
 
-	return *(u32 *)desc.ctx;
+	return *desc.ctx;
 }
 
 EXPORT_SYMBOL(crc32c);
