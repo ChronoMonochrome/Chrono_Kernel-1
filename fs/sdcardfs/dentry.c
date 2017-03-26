ec74f87467176720c7b8482460ce49c7a49b74ce
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-26 09:15:47 +0200

    sdcardfs

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -182,6 +182,8 @@ const struct dentry_operations sdcardfs_ci_dops = {
 	.d_release	= sdcardfs_d_release,
 	.d_hash 	= sdcardfs_hash_ci,
 	.d_compare	= sdcardfs_cmp_ci,
+#if 0
 	.d_canonical_path = sdcardfs_canonical_path,
+#endif
 };
 
