a03efddc2537b3bdc45ad96f37d5994db2586b0d
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-27 08:11:34 +0200

    mcde: squash update to master

--- drivers/video/mcde/display-s6d27a1_dpi.c
+++ drivers/video/mcde/display-s6d27a1_dpi.c
@@ -86,7 +86,8 @@
 
 #define DPI_DISP_TRACE	dev_dbg(&ddev->dev, "%s\n", __func__)
 
-static signed char apeopp_requirement = 0, ddropp_requirement = 0;
+static signed char apeopp_requirement = 50, ddropp_requirement = 50;
+static unsigned int reset_delay = 5, power_on_delay = 5;
 
 /* to be removed when display works */
 //#define dev_dbg	dev_info
@@ -660,7 +661,7 @@ static int s6d27a1_dpi_power_on(struct s6d27a1_dpi *lcd)
 	}
 
 	dpd->power_on(dpd, LCD_POWER_UP);
-	msleep(dpd->power_on_delay);
+	msleep(power_on_delay);
 
 	if (!dpd->gpio_cfg_lateresume) {
 		dev_err(lcd->dev, "gpio_cfg_lateresume is NULL.\n");
@@ -669,7 +670,7 @@ static int s6d27a1_dpi_power_on(struct s6d27a1_dpi *lcd)
 		dpd->gpio_cfg_lateresume();
 
 	dpd->reset(dpd);
-	msleep(dpd->reset_delay);
+	msleep(reset_delay);
 
 	ret = s6d27a1_dpi_ldi_init(lcd);
 	if (ret) {
@@ -946,6 +947,253 @@ static DEVICE_ATTR(mcde_screenon_opp, 0644,
 		s6d_sysfs_show_opp, s6d_sysfs_store_opp);
 
 
+static ssize_t sysfs_show_display_settings(struct device *dev,
+				      struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "power_on_delay=%d\n"
+			    "reset_delay=%d\n",
+			    power_on_delay,
+			    reset_delay);
+}
+
+static ssize_t sysfs_store_display_settings(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t len)
+{
+	int val;
+  
+  	if (!strncmp(&buf[0], "power_on_delay=", 15))
+	{
+		sscanf(&buf[15], "%d", &val);
+		
+		power_on_delay = val;
+		
+		return len;
+	}
+	
+	if (!strncmp(&buf[0], "reset_delay=", 12))
+	{
+		sscanf(&buf[12], "%d", &val);
+		
+		reset_delay = val;
+
+		return len;
+	}
+	
+	return -EINVAL;
+}
+static DEVICE_ATTR(display_settings, 0644,
+		sysfs_show_display_settings, sysfs_store_display_settings);
+
+static ssize_t s6d27a1_sysfs_show_mcde_chnl(struct device *dev,
+				      struct device_attribute *attr, char *buf)
+{
+	struct s6d27a1_dpi *lcd = dev_get_drvdata(dev);
+
+	sprintf(buf,   "[s6d27a1 MCDE Channel]\n");
+	sprintf(buf, "%spixclock: %d\n", buf, lcd->mdd->video_mode.pixclock);
+	sprintf(buf, "%shbp: %d\n", buf, lcd->mdd->video_mode.hbp);
+	sprintf(buf, "%shfp: %d\n", buf, lcd->mdd->video_mode.hfp);
+	sprintf(buf, "%shsw: %d\n", buf, lcd->mdd->video_mode.hsw);
+	sprintf(buf, "%svbp: %d\n", buf, lcd->mdd->video_mode.vbp);
+	sprintf(buf, "%svfp: %d\n", buf, lcd->mdd->video_mode.vfp);
+	sprintf(buf, "%svsw: %d\n", buf, lcd->mdd->video_mode.vsw);
+	sprintf(buf, "%sinterlaced: %d\n", buf, lcd->mdd->video_mode.interlaced);
+
+	return strlen(buf);
+}
+
+static ssize_t s6d27a1_sysfs_store_mcde_chnl(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t len)
+{
+	struct s6d27a1_dpi *lcd = dev_get_drvdata(dev);
+	int ret;
+	u32 pclk;	/* pixel clock in ps (pico seconds) */
+	u32 hbp;	/* horizontal back porch: left margin (excl. hsync) */
+	u32 hfp;	/* horizontal front porch: right margin (excl. hsync) */
+	u32 hsw;	/* horizontal sync width */
+	u32 vbp;	/* vertical back porch: upper margin (excl. vsync) */
+	u32 vfp;	/* vertical front porch: lower margin (excl. vsync) */
+	u32 vsw;	/* vertical sync width */
+	u32 interlaced;
+	u32 enable;
+
+	if (!strncmp(buf, "set_vmode", 8))
+	{
+		pr_err("[s6d27a1] Save chnl params\n");
+		mcde_chnl_set_video_mode(lcd->mdd->chnl_state, &lcd->mdd->video_mode);
+
+		return len;
+	}
+
+	if (!strncmp(buf, "apply_config", 8)) 
+	{
+		pr_err("[s6d27a1] Apply chnl config!\n");
+		mcde_chnl_apply(lcd->mdd->chnl_state);
+		
+		return len;
+	}
+
+	if (!strncmp(buf, "stop_flow", 8)) 
+	{
+		pr_err("[s6d27a1] MCDE chnl stop flow!\n");
+		mcde_chnl_stop_flow(lcd->mdd->chnl_state);
+		
+		return len;
+	}
+
+	if (!strncmp(buf, "update", 6)) 
+	{
+		pr_err("[s6d27a1] Update MCDE chnl!\n");
+		mcde_chnl_set_video_mode(lcd->mdd->chnl_state, &lcd->mdd->video_mode);
+		mcde_chnl_apply(lcd->mdd->chnl_state);
+		mcde_chnl_stop_flow(lcd->mdd->chnl_state);
+		
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "enable=", 7))
+	{
+		sscanf(&buf[7], "%d", &enable);
+		pr_err("[s6d27a1] %s chnl\n", enable ? "Enable" : "Disable");
+
+		if (!enable)
+			mcde_chnl_disable(lcd->mdd->chnl_state);
+		else
+			mcde_chnl_enable(lcd->mdd->chnl_state);
+		
+		return len;
+	}
+	
+	if (!strncmp(&buf[0], "pclk=", 5))
+	{
+		ret = sscanf(&buf[5], "%d", &pclk);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] pclk: %d\n", pclk);
+		lcd->mdd->video_mode.pixclock = pclk;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "hbp=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &hbp);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] hbp: %d\n", hbp);
+		lcd->mdd->video_mode.hbp = hbp;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "hfp=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &hfp);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] hfp: %d\n", hfp);
+		lcd->mdd->video_mode.hfp = hfp;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "hsw=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &hsw);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] hsw: %d\n", hsw);
+		lcd->mdd->video_mode.hsw = hsw;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "vbp=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &vbp);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] vbp: %d\n", vbp);
+		lcd->mdd->video_mode.vbp = vbp;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "vfp=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &vfp);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] vfp: %d\n", vfp);
+		lcd->mdd->video_mode.vfp = vfp;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "vsw=", 4))
+	{
+		ret = sscanf(&buf[4], "%d", &vsw);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] vsw: %d\n", vsw);
+		lcd->mdd->video_mode.vsw = vsw;
+
+		return len;
+	}
+
+	if (!strncmp(&buf[0], "interlaced=", 11))
+	{
+		ret = sscanf(&buf[11], "%d", &interlaced);
+		if (!ret) {
+			pr_err("[s6d27a1] Invaild param\n");
+	
+			return -EINVAL;
+		}
+
+		pr_err("[s6d27a1] interlaced: %d\n", interlaced);
+		lcd->mdd->video_mode.interlaced = interlaced;
+
+		return len;
+	}
+
+	pr_err("[s6d27a1] Invaild cmd\n");
+
+	return len;
+}
+
+static DEVICE_ATTR(mcde_chnl, 0644,
+		s6d27a1_sysfs_show_mcde_chnl, s6d27a1_sysfs_store_mcde_chnl);
+
 static ssize_t s6d27a1_dpi_sysfs_store_lcd_power(struct device *dev,
 						struct device_attribute *attr,
 						const char *buf, size_t len)
@@ -1019,9 +1267,19 @@ static void est_test_timer_func(unsigned long data)
 }
 #endif
 #endif
+
+static bool s6d = 0;
+bool is_s6d(void)
+{
+	return s6d;
+}
+
 static int __devinit s6d27a1_dpi_spi_probe(struct spi_device *spi)
 {
 	int ret = 0;
+
+	s6d = 1;
+	
 	struct s6d27a1_dpi *lcd = container_of(spi->dev.driver,
 					 struct s6d27a1_dpi, spi_drv.driver);
 
@@ -1189,7 +1447,15 @@ static int __devinit s6d27a1_dpi_mcde_probe(
 		
 	ret = device_create_file(&(ddev->dev), &dev_attr_mcde_screenon_opp);	
 	if (ret < 0)
-		dev_err(&(ddev->dev), "failed to add mcde_screeon_opp sysfs entries\n");
+		dev_err(&(ddev->dev), "failed to add mcde_screenon_opp sysfs entries\n");
+	
+	ret = device_create_file(&(ddev->dev), &dev_attr_display_settings);	
+	if (ret < 0)
+		dev_err(&(ddev->dev), "failed to add display_settings sysfs entries\n");
+
+ret = device_create_file(&(ddev->dev), &dev_attr_mcde_chnl);
+	if (ret < 0)
+		dev_err(&(ddev->dev), "failed to add sysfs entries\n");
 
 	lcd->spi_drv.driver.name	= "pri_lcd_spi";
 	lcd->spi_drv.driver.bus		= &spi_bus_type;
