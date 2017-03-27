d02bb016be6092f035413afcb50d8d11fd03a08a
Shilin Victor <chrono.monochrome@gmail.com>
2017-03-27 09:20:38 +0200

    block: squashed update to master

--- block/partitions/check.c
+++ block/partitions/check.c
@@ -22,7 +22,6 @@
 #include "acorn.h"
 #include "amiga.h"
 #include "atari.h"
-#include "blkdev_parts.h"
 #include "ldm.h"
 #include "mac.h"
 #include "msdos.h"
@@ -42,9 +41,6 @@ static int (*check_part[])(struct parsed_partitions *) = {
 	 * Probe partition formats with tables at disk address 0
 	 * that also have an ADFS boot block at 0xdc0.
 	 */
-#ifdef CONFIG_BLKDEV_PARTITION
-	blkdev_partition,
-#endif
 #ifdef CONFIG_ACORN_PARTITION_ICS
 	adfspart_check_ICS,
 #endif
