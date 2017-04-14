e15ae74e1d27d333ece3bdab3225fb82b4aa16bc
Shilin Victor <chrono.monochrome@gmail.com>
2017-04-14 13:41:05 +0300

    Revert "UPSTREAM: arm: don't open-code ptrace_report_syscall()"
    This reverts commit 518b40696b5de6b07759ddd40e61cefbf1ee65f8.

--- arch/arm/kernel/ptrace.c
+++ arch/arm/kernel/ptrace.c
@@ -934,7 +934,20 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 	 */
 	ip = regs->ARM_ip;
 	regs->ARM_ip = why;
-	ptrace_report_syscall(regs);
+
+	/* the 0x80 provides a way for the tracing parent to distinguish
+	   between a syscall stop and SIGTRAP delivery */
+	ptrace_notify(SIGTRAP | ((current->ptrace & PT_TRACESYSGOOD)
+				 ? 0x80 : 0));
+	/*
+	 * this isn't the same as continuing with a signal, but it will do
+	 * for normal use.  strace only continues with a signal if the
+	 * stopping signal is not SIGTRAP.  -brl
+	 */
+	if (current->exit_code) {
+		send_sig(current->exit_code, current, 1);
+		current->exit_code = 0;
+	}
 	regs->ARM_ip = ip;
 
 	return current_thread_info()->syscall;
