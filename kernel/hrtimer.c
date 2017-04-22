bcd1de5d0360e4b78f65f1e694eb095505ed5310
Shilin Victor <chrono.monochrome@gmail.com>
2017-04-22 22:11:36 +0300

    Revert "nanosleep: use freezable blocking call"
    This reverts commit 30e3532819b763176dd84a2d135a9387de677bec.

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -45,7 +45,6 @@
 #include <linux/debugobjects.h>
 #include <linux/sched.h>
 #include <linux/timer.h>
-#include <linux/freezer.h>
 
 #include <asm/uaccess.h>
 
@@ -1532,7 +1531,7 @@ static int __sched do_nanosleep(struct hrtimer_sleeper *t, enum hrtimer_mode mod
 			t->task = NULL;
 
 		if (likely(t->task))
-			freezable_schedule();
+			schedule();
 
 		hrtimer_cancel(&t->timer);
 		mode = HRTIMER_MODE_ABS;
