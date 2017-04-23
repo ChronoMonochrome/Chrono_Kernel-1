757640d546bbd2022dd8c67c15ae2a373bf2e5f4
Shilin Victor <chrono.monochrome@gmail.com>
2017-04-23 04:16:53 +0300

    power: update wakeup code to 3.4

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -352,7 +352,7 @@ extern int unregister_pm_notifier(struct notifier_block *nb);
 extern bool events_check_enabled;
 
 extern bool pm_wakeup_pending(void);
-extern bool pm_get_wakeup_count(unsigned int *count);
+extern bool pm_get_wakeup_count(unsigned int *count, bool block);
 extern bool pm_save_wakeup_count(unsigned int count);
 #else /* !CONFIG_PM_SLEEP */
 
