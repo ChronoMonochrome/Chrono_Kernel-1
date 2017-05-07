84e04c28a7cb951f15d2fbde91fc43efe38a95d3
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-07 23:03:24 +0300

    Revert "arm: Use generic idle loop"
    This reverts commit f7b861b7a6d9d1838cbbb5f4053e61578b86d134.

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -369,7 +369,7 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	/*
 	 * OK, it's off to the idle thread for us
 	 */
-	cpu_startup_entry(CPUHP_ONLINE);
+	cpu_idle();
 }
 
 void __init smp_cpus_done(unsigned int max_cpus)
--- arch/arm/mach-shmobile/suspend.c
+++ arch/arm/mach-shmobile/suspend.c
@@ -12,8 +12,6 @@
 #include <linux/suspend.h>
 #include <linux/module.h>
 #include <linux/err.h>
-#include <linux/cpu.h>
-
 #include <asm/io.h>
 #include <asm/system_misc.h>
 
@@ -25,13 +23,13 @@ static int shmobile_suspend_default_enter(suspend_state_t suspend_state)
 
 static int shmobile_suspend_begin(suspend_state_t state)
 {
-	cpu_idle_poll_ctrl(true);
+	disable_hlt();
 	return 0;
 }
 
 static void shmobile_suspend_end(void)
 {
-	cpu_idle_poll_ctrl(false);
+	enable_hlt();
 }
 
 struct platform_suspend_ops shmobile_suspend_ops = {
