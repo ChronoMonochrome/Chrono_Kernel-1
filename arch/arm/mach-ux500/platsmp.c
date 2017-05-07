c69cb1b87c992af0c0ac3dac1bdc3ab35ed238c2
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-07 21:26:42 +0300

    ux500: GIC: remove direct use of gic_raise_softirq

--- arch/arm/mach-ux500/platsmp.c
+++ arch/arm/mach-ux500/platsmp.c
@@ -100,7 +100,7 @@ static int __cpuinit ux500_boot_secondary(unsigned int cpu, struct task_struct *
 	 */
 	write_pen_release(cpu_logical_map(cpu));
 
-	smp_send_reschedule(cpu);
+	arch_send_wakeup_ipi_mask(cpumask_of(cpu));
 
 	timeout = jiffies + (1 * HZ);
 	while (time_before(jiffies, timeout)) {
@@ -166,8 +166,6 @@ static void __init ux500_smp_init_cpus(void)
 
 	for (i = 0; i < ncores; i++)
 		set_cpu_possible(i, true);
-
-	set_smp_cross_call(gic_raise_softirq);
 }
 
 static void __init ux500_smp_prepare_cpus(unsigned int max_cpus)
