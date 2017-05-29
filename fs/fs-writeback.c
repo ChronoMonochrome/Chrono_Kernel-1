f38630c8238304768b750e3dcc1245e1b0b01de2
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-29 18:22:22 +0300

    Revert "pm: reset to LK3.0"
    This reverts commit e8432f5e237e5be18b4a6f097ed5a47191db397a.

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -950,7 +950,7 @@ int bdi_writeback_thread(void *data)
 
 	trace_writeback_thread_start(bdi);
 
-	while (!kthread_should_stop()) {
+	while (!kthread_freezable_should_stop(NULL)) {
 		/*
 		 * Remove own delayed wake-up timer, since we are already awake
 		 * and we'll take care of the preriodic write-back.
@@ -980,8 +980,6 @@ int bdi_writeback_thread(void *data)
 			 */
 			schedule();
 		}
-
-		try_to_freeze();
 	}
 
 	/* Flush any work that raced with us exiting */
