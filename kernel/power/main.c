5e361c6d5b2934e632928dcc656e1fdf0fc7e0b0
Shilin Victor <chrono.monochrome@gmail.com>
2017-06-12 12:20:24 +0300

    power: forward port various drivers to android 3.8

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -313,7 +313,11 @@ static ssize_t state_show(struct kobject *kobj, struct kobj_attribute *attr,
 static suspend_state_t decode_state(const char *buf, size_t n)
 {
 #ifdef CONFIG_SUSPEND
+#ifdef CONFIG_EARLYSUSPEND
+	suspend_state_t state = PM_SUSPEND_ON;
+#else
 	suspend_state_t state = PM_SUSPEND_STANDBY;
+#endif	
 	const char * const *s;
 #endif
 	char *p;
@@ -352,7 +356,14 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 
 	state = decode_state(buf, n);
 	if (state < PM_SUSPEND_MAX)
+#ifndef CONFIG_EARLYSUSPEND
 		error = pm_suspend(state);
+#else
+	{
+		error = 0;
+		request_suspend_state(state);
+	}
+#endif
 	else if (state == PM_SUSPEND_MAX)
 		error = hibernate();
 	else
@@ -472,43 +483,6 @@ static ssize_t autosleep_store(struct kobject *kobj,
 
 power_attr(autosleep);
 #endif /* CONFIG_PM_AUTOSLEEP */
-
-#ifdef CONFIG_PM_WAKELOCKS
-static ssize_t wake_lock_show(struct kobject *kobj,
-			      struct kobj_attribute *attr,
-			      char *buf)
-{
-	return pm_show_wakelocks(buf, true);
-}
-
-static ssize_t wake_lock_store(struct kobject *kobj,
-			       struct kobj_attribute *attr,
-			       const char *buf, size_t n)
-{
-	int error = pm_wake_lock(buf);
-	return error ? error : n;
-}
-
-power_attr(wake_lock);
-
-static ssize_t wake_unlock_show(struct kobject *kobj,
-				struct kobj_attribute *attr,
-				char *buf)
-{
-	return pm_show_wakelocks(buf, false);
-}
-
-static ssize_t wake_unlock_store(struct kobject *kobj,
-				 struct kobj_attribute *attr,
-				 const char *buf, size_t n)
-{
-	int error = pm_wake_unlock(buf);
-	return error ? error : n;
-}
-
-power_attr(wake_unlock);
-
-#endif /* CONFIG_PM_WAKELOCKS */
 #endif /* CONFIG_PM_SLEEP */
 
 #ifdef CONFIG_PM_TRACE
@@ -550,8 +524,12 @@ pm_trace_dev_match_store(struct kobject *kobj, struct kobj_attribute *attr,
 }
 
 power_attr(pm_trace_dev_match);
-
 #endif /* CONFIG_PM_TRACE */
+
+#ifdef CONFIG_USER_WAKELOCK
+power_attr(wake_lock);
+power_attr(wake_unlock);
+#endif
 
 static struct attribute * g[] = {
 	&state_attr.attr,
@@ -565,7 +543,7 @@ static struct attribute * g[] = {
 #ifdef CONFIG_PM_AUTOSLEEP
 	&autosleep_attr.attr,
 #endif
-#ifdef CONFIG_PM_WAKELOCKS
+#ifdef CONFIG_USER_WAKELOCK
 	&wake_lock_attr.attr,
 	&wake_unlock_attr.attr,
 #endif
