314214214f6571f7e5ebecf0042c590991ad9585
Shilin Victor <chrono.monochrome@gmail.com>
2017-06-12 15:53:21 +0300

    Revert "fs: use read_iter and write_iter rather than aio_read and aio_write"
    This reverts commit 3c321fb8679a0af363b4f86912f8e36da8607527.

--- fs/aio.c
+++ fs/aio.c
@@ -39,8 +39,6 @@
 #include <asm/kmap_types.h>
 #include <asm/uaccess.h>
 
-#include "read_write.h"
-
 #if DEBUG > 1
 #define dprintk		printk
 #else
@@ -1328,10 +1326,10 @@ static ssize_t aio_rw_vect_retry(struct kiocb *iocb)
 
 	if ((iocb->ki_opcode == IOCB_CMD_PREADV) ||
 		(iocb->ki_opcode == IOCB_CMD_PREAD)) {
-		rw_op = do_aio_read;
+		rw_op = file->f_op->aio_read;
 		opcode = IOCB_CMD_PREADV;
 	} else {
-		rw_op = do_aio_write;
+		rw_op = file->f_op->aio_write;
 		opcode = IOCB_CMD_PWRITEV;
 	}
 
@@ -1460,7 +1458,7 @@ static ssize_t aio_setup_iocb(struct kiocb *kiocb, bool compat)
 		if (ret)
 			break;
 		ret = -EINVAL;
-		if (file->f_op->read_iter || file->f_op->aio_read)
+		if (file->f_op->aio_read)
 			kiocb->ki_retry = aio_rw_vect_retry;
 		break;
 	case IOCB_CMD_PWRITE:
@@ -1475,7 +1473,7 @@ static ssize_t aio_setup_iocb(struct kiocb *kiocb, bool compat)
 		if (ret)
 			break;
 		ret = -EINVAL;
-		if (file->f_op->write_iter || file->f_op->aio_write)
+		if (file->f_op->aio_write)
 			kiocb->ki_retry = aio_rw_vect_retry;
 		break;
 	case IOCB_CMD_PREADV:
@@ -1486,7 +1484,7 @@ static ssize_t aio_setup_iocb(struct kiocb *kiocb, bool compat)
 		if (ret)
 			break;
 		ret = -EINVAL;
-		if (file->f_op->read_iter || file->f_op->aio_read)
+		if (file->f_op->aio_read)
 			kiocb->ki_retry = aio_rw_vect_retry;
 		break;
 	case IOCB_CMD_PWRITEV:
@@ -1497,7 +1495,7 @@ static ssize_t aio_setup_iocb(struct kiocb *kiocb, bool compat)
 		if (ret)
 			break;
 		ret = -EINVAL;
-		if (file->f_op->write_iter || file->f_op->aio_write)
+		if (file->f_op->aio_write)
 			kiocb->ki_retry = aio_rw_vect_retry;
 		break;
 	case IOCB_CMD_FDSYNC:
