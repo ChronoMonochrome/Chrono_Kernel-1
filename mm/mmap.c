59641173793945053c03e98f60814e28c129a852
Shilin Victor <chrono.monochrome@gmail.com>
2017-06-12 16:15:03 +0300

    Revert "sdcardfs: Flag files as non-mappable"
    This reverts commit bd0464bb1db56dde94dc0b1d216c99eeac54660e.

--- mm/mmap.c
+++ mm/mmap.c
@@ -1159,9 +1159,6 @@ unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	struct inode *inode;
 	vm_flags_t vm_flags;
 
-	while (file && (file->f_mode & FMODE_NONMAPPABLE))
-		file = file->f_op->get_lower_file(file);
-
 	/*
 	 * Does the application expect PROT_READ to imply PROT_EXEC?
 	 *
