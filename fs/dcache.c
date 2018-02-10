628897e7f8a9904e7ddccb28290151aa38253d93
Shilin Victor <chrono.monochrome@gmail.com>
2018-02-11 01:42:09 +0300

    Revert "dcache: Fix locking bugs in backported "deal with deadlock in d_walk()""
    This reverts commit c190d250d8db5620218d5d56999580ed8488ec24.

--- fs/dcache.c
+++ fs/dcache.c
@@ -1072,7 +1072,7 @@ ascend:
 	return 0; /* No mount points found in tree */
 positive:
 	if (!locked && read_seqretry(&rename_lock, seq))
-		goto rename_retry_unlocked;
+		goto rename_retry;
 	if (locked)
 		write_sequnlock(&rename_lock);
 	return 1;
@@ -1082,7 +1082,6 @@ rename_retry:
 	rcu_read_unlock();
 	if (locked)
 		goto again;
-rename_retry_unlocked:
 	locked = 1;
 	write_seqlock(&rename_lock);
 	goto again;
@@ -1147,7 +1146,6 @@ resume:
 		 */
 		if (found && need_resched()) {
 			spin_unlock(&dentry->d_lock);
-			rcu_read_lock();
 			goto out;
 		}
 
